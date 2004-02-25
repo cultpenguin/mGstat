dab9da90cc83f73cd9b6ce22215d1dad0c0ad752
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2004-02-25 10:25:31 +0000

    Semivariogram

--- trunk/mGstat/semivar_synth.m
+++ trunk/mGstat/semivar_synth.m
@@ -1,13 +1,21 @@
 % semivar_synth : synthethic semivariogram
+%
+% Call ex :  
+%    [sv,d]=semivar_synth('0.1 Nug(0) + 1 Gau(1.5)',[0:.1:6]);plot(d,sv)
+% or : 
+%    V(1).par1=1;V(1).par2=1.5;V(1).type='Gau';
+%    V(2).par1=0.1;V(2).par2=0;V(2).type='Nug';
+%    [sv,d]=semivar_synth(V,[0:.1:6]);plot(d,sv)
+%
 function [sv,d]=semivar_synth(V,d);
   
   if nargin==0,
-    V='5 Nug(0) + 1 Sph(5)';
-    d=[0:1:100];
+    V='5 Nug(0) + 1 Sph(5)'
+    d=[0:.1:20];
   end
   
   if nargin==1
-    d=[0:1:100];
+    d=[0:.1:20];
   end
   
   if isstr(V)
@@ -17,20 +25,51 @@ function [sv,d]=semivar_synth(V,d);
   sv=zeros(size(d));
   
   for iv=1:length(V),
-    keyboard
-   
-    synthetic_variogram(V(iv).type,V(iv).par1,V(iv).par2);
-    
+    [gamma]=synthetic_variogram(V(iv),d);
+    sv=sv+gamma;
   end
   
-function synthetic_variogram(type,v1,v2)
+function [gamma,h]=synthetic_variogram(V,h)
+  
+  type=V.type;
+  v1=V.par1;
+  v2=V.par2;
+  gamma=h.*0;
+  
+  s1=find(h<v2);
+  s2=find(h>=v2);      
+  
   if strmatch(type,'Nug')
-    disp('Nug')
+    mgstat_verbose('Nug',12);
+    gamma(1)=0;
+    gamma(2:length(h))=v1;
     %% SEE GSTAT MANUAL FOR TYPES....
   elseif strmatch(type,'Sph')
-    disp('Sph')
-  elseif strmatch(type,'Gauss')
+    mgstat_verbose('Sph',12);
+    gamma(s1)=v1.*(1.5*abs(h(s1))/(v2) - .5* (h(s1)./v2).^3);
+    gamma(s2)=v1;
+  elseif strmatch(type,'Gau')
+    mgstat_verbose('Gau',12);
+    gamma=v1.*(1-exp(-(h./v2).^2));
+  elseif strmatch(type,'Lin')
+    mgstat_verbose('Lin',12);
+    if v2==0,
+      gamma=v1.*h;
+    else
+      gamma(s1)=h(s1)./v2;
+      gamma(s2)=1;
+      gamma=gamma.*v1;
+    end
+  elseif strmatch(type,'Log')
+    mgstat_verbose(type,12);
+    gamma=log(h+v2);
+  elseif strmatch(type,'Pow')
+    mgstat_verbose(type,12);
+    gamma=h.^v2;
+  elseif strmatch(type,'Exp')
+    mgstat_verbose(type,12);
+    gamma=1-exp(-h/v2);
   else
-    mgstat_verbose(sprintf('%s : ''%s'' type is not recognized',mfilename,type));
+    mgstat_verbose(sprintf('%s : ''%s'' type is not recognized',mfilename,type),-1);
   end
   
