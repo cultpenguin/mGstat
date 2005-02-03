37898def9e406fbe55a9a3c51da32a73cf5303fa
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2005-02-03 09:04:54 +0000

    Cleaning up for 1.0 release.

--- deformat_variogram.m
+++ deformat_variogram.m
@@ -11,35 +11,40 @@ function V=deformat_variogram(txt);
   txt=regexprep(txt,'\<;','');
   
   % HERE IS A BUG WHEN SOMETHING LIKE 'e+24 is used!!!
+	% find position of '+'
   fp=regexp(txt,'\+');
   
- 
   nvar=length(fp)+1;
-  mgstat_verbose(sprintf('Found %d variograms',nvar),10)  
+  disp(sprintf('Found %d variograms',nvar))  
 
   if  nvar==1,
     ifp_array=[0];
   else
     ifp_array=[0 fp];
   end
+	
   ivar=0;  
-  for ifp=ifp_array;
+  for ifp=1:length(ifp_array);
     ivar=ivar+1;
-    
     if nvar==1,
       vartxt=txt;
     else
-      if ifp==0,
+      if ifp_array(ifp)==0,
         vartxt=txt(1:fp(1)-1);
-      elseif ifp==fp 
-        vartxt=txt(ifp+1:length(txt));
-      else
-        mgstat_verbose('MORE THAN TWO VARIOGRAMS NOT IMPLEMENTED YET',0)
+      elseif ifp_array(ifp)==max(fp)
+        vartxt=txt(ifp_array(ifp)+1:length(txt));
+			else
+				ind1=ifp_array(ifp)+1;
+				ind2=ifp_array(ifp+1)-1;
+        vartxt=txt(ind1:ind2);				
+				%      else
+			%	keyboard
+   %     mgstat_verbose('MORE THAN TWO VARIOGRAMS NOT IMPLEMENTED YET',0)
       end
     end
     
     vartxt=strip_space(vartxt);
-    mgstat_verbose(sprintf('V%d :  %s',ivar,vartxt),11)
+	  %disp(sprintf('ivar=%d ifp=%d --%s--',ivar,ifp,vartxt))
     
     sp=find(vartxt==' ');
     lb=find(vartxt=='(');
@@ -53,10 +58,27 @@ function V=deformat_variogram(txt);
     if ~isempty(str2num(par2)), par2=str2num(par2); end
     
     if isempty(par2), par2=[];end
-    
+
+		if (strcmp(type,'Nug'))
+			itype=0;
+		elseif (strcmp(type,'Sph')),
+			itype=1;
+		elseif (strcmp(type,'Gau'))
+			itype=2;
+		elseif (strcmp(type,'Sph')),
+			itype=3;
+		elseif (strcmp(type,'Exp'))
+			itype=4;
+		elseif (strcmp(type,'Lin'))
+			disp(['Unknown type : ',type])
+			V(ivar).itype=5;			
+		end
+		
+		% disp(sprintf('par1=%5.1f par2=%5.1f type=%4s itype=%d',par1,par2,type,itype))
     
     V(ivar).par1=par1;
     V(ivar).par2=par2;
     V(ivar).type=type;
+    V(ivar).itype=itype;
     
   end
