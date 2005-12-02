9dea58c86cb4ff960f980a88545e13dd2100b325
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2005-12-02 13:37:41 +0000

    *** empty log message ***

--- trunk/mGstat/gstat_ex/mgstat_gstatex.m
+++ trunk/mGstat/gstat_ex/mgstat_gstatex.m
@@ -1,7 +1,7 @@
-mgstat_convert('mask_map');
-mgstat_convert('part_a');
-mgstat_convert('part_b');
-mgstat_convert('sqrtdist');
+gstat_convert('mask_map');
+gstat_convert('part_a');
+gstat_convert('part_b');
+gstat_convert('sqrtdist');
 
 if isunix
   unix('cp mask_map.ascii mask_map');
@@ -17,26 +17,26 @@ end
 
 
 figure;
-mgstat_plot('ex03.cmd');
+gstat_plot('ex03.cmd');
 figure;
-mgstat_plot('ex05.cmd');
+gstat_plot('ex05.cmd');
 figure;
-mgstat_plot('ex07.cmd');
+gstat_plot('ex07.cmd');
 figure;
-mgstat_plot('ex08.cmd');
+gstat_plot('ex08.cmd');
 figure;
-mgstat_plot('ex10.cmd');
+gstat_plot('ex10.cmd');
 figure;
-mgstat_plot('ex11.cmd');
+gstat_plot('ex11.cmd');
 figure;
-mgstat_plot('ex12.cmd');
+gstat_plot('ex12.cmd');
 figure;
-mgstat_plot('ex13.cmd');
+gstat_plot('ex13.cmd');
 figure;
-mgstat_plot('ex14.cmd');
+gstat_plot('ex14.cmd');
 figure;
-mgstat_plot('ex15.cmd');
+gstat_plot('ex15.cmd');
 figure;
-mgstat_plot('ex16.cmd');
+gstat_plot('ex16.cmd');
 figure;
-mgstat_plot('ex17.cmd');
+gstat_plot('ex17.cmd');
