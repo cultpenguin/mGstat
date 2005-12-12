d3a83a50cb66d47fec3dc07e64939a8246d9102a
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2005-12-12 10:12:36 +0000

    *** empty log message ***

--- print_mul.m
+++ print_mul.m
@@ -1,13 +1,50 @@
-% print both EPS and PNG files
-function print_mul(fname);
+% print_mul : prints both EPS and PNG figures of current plot
+%
+%
+% Call :
+%   print_mul('test') : prints test.eps and test.png
+%
+% In case 'mogrify' is available on the system
+% the png file will be trimmed and optionall 
+% A specific color will be made transparent :
+%
+%   print_mul('test',red) 
+%       also creates trim_test.png
+%
+%   print_mul('test',1) 
+%       also creates trim_test.png, with transparent white color
+%
+%   print_mul('test','red') 
+%       also creates trim_test.png, with transparent red color
+%
+% /TMH 12/2005
+%
 
-  print(gcf, '-dpng', fname )
+function print_mul(fname,color);
+  print(gcf, '-depsc2', [fname,'.eps'] )
+  print(gcf, '-dpng','-r300', [fname,'.png'] )
 
-  print(gcf, '-depsc2', fname )
+  [a,mogrifybin]=unix('which mogrify');
+  mogrifybin=mogrifybin(1:length(mogrifybin)-1);
+  fname_trim=sprintf('trim_%s',fname);
   
-  trim_cmd='/home/tmh/bin/trim_image';
-  
-  if exist(trim_cmd)==2
-    system(sprintf('%s %s.png',trim_cmd,fname));
+  if exist(mogrifybin)==2,
+        
+    system(sprintf('cp %s.png %s.png',fname,fname_trim));    
+    if nargin==1
+      system(sprintf('%s -trim %s.png',mogrifybin,fname_trim));
+      
+    else
+      if isnumeric(color)
+        if color==1;
+          color='white';
+        else
+          return
+        end
+      end
+      
+      system(sprintf('%s -trim -transparent %s %s.png',mogrifybin,color,fname_trim));
+    end
   end
+  
   
