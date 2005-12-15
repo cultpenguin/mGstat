ffe88d348884501941d2d0479491b5de2a86b1ce
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2005-12-15 11:28:36 +0000

    *** empty log message ***

--- trunk/mGstat/krig_optim_range.m
+++ trunk/mGstat/krig_optim_range.m
@@ -2,7 +2,11 @@
 % CALL : 
 %   [V,be]=krig_optim_range(pos_known,val_known,V,options)
 %
-function [V,be]=krig_optim_range(pos_known,val_known,V,options);
+function [V,be_acc,V_acc]=krig_optim_range(pos_known,val_known,V,options);
+
+if isstr(V),
+  V=deformat_variogram(V);
+end 
 
 ndim=size(pos_known,2);
 
@@ -16,6 +20,8 @@ nugarr=linspace(0,1,nnug);nugarr(1)=.01;
 std_known=std(pos_known);
 mean_known=mean(pos_known);
 
+
+% A PRIORI 
 na=25;
 for idim=1:ndim
   narr{idim}=na;
@@ -23,7 +29,55 @@ for idim=1:ndim
   arr{idim}(1)=0.01;
 end
 
-Vorig=V;
+V_init=V;
+V_old=V;
+[d1,d2,be]=gstat_krig_blinderror(pos_known,val_known,pos_known,V_init,options);
+be_old=be;
+be_init=be;
+
+be_arr=[];
+
+be_min=0.8*be;
+
+nacc=0;
+for i=1:100
+  
+  % perturb model 
+  V_new = V_old;
+  V_new(2).par2=V_new(2).par2.*(1+rand(size(V_new(2).par2))/1);
+  
+  [d1,d2,be_new]=gstat_krig_blinderror(pos_known,val_known,pos_known,V_new,options);
+
+  
+  PA=min(1,be_old/be_new);
+
+%  disp(sprintf('be = %6.3g  , PA=%6.2g : %s',be_new,PA,format_variogram(V_new)))
+  
+  Prand=rand(1);
+  Prand=1;
+  
+  be_arr(i)=be_new;
+  %plot(be_arr);drawnow;
+  
+  if PA>Prand
+    nacc=nacc+1;
+
+    be_acc(nacc) = be_new;
+    V_acc{nacc} = V_new; 
+    plot(be_acc);drawnow;
+   
+    V_old=V_new;
+    be_old=be_new;
+    disp(sprintf('%3d --OK-- be = %6.3g  , PA=%4.2g Prand=%4.2g : %s',i,be_new,PA,Prand,format_variogram(V_new)))
+  else
+    disp(sprintf('%3d ------ be = %6.3g  , PA=%4.2g Prand=%4.2g : %s',i,be_new,PA,Prand,format_variogram(V_new)))
+  end
+  % 
+  
+end
+
+
+return
 
 for in=1:nnug
   dnug=nugarr(in);
