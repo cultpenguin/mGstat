b15bb2e71020284cbcbfa3eb3fa5d59fd5585756
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2005-12-16 09:17:37 +0000

    *** empty log message ***

--- trunk/mGstat/gstat_krig_blinderror.m
+++ trunk/mGstat/gstat_krig_blinderror.m
@@ -8,7 +8,7 @@
 %
 % /TMH 12/2005
 %
-function [d_est,d_var,be,d_diff]=gstat_krig_blinderror(pos_known,val_known,pos_est,V,options);
+function [d_est,d_var,be,d_diff,L]=gstat_krig_blinderror(pos_known,val_known,pos_est,V,options);
     
   if isstr(V),
     V=deformat_variogram(V);
@@ -20,3 +20,14 @@ function [d_est,d_var,be,d_diff]=gstat_krig_blinderror(pos_known,val_known,pos_e
   d_diff=d_est-val_known(:,1);
   be=mean(abs(d_diff));
   
+  
+  if nargout==5
+    % CALULATE LIKELIHOOD
+    nd=size(val_known,1);
+    Cd=zeros(nd,nd);
+    for i=1:nd
+      Cd(i,i)=d_var(i);
+    end
+    L=exp(-.5*d_diff'*inv(Cd)*d_diff);
+    
+  end
