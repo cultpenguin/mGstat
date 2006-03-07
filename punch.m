8997318fc8e19dd632e80dc57f59de225d6d755a
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2006-03-07 14:04:24 +0000

    *** empty log message ***

---
+++ punch.m
@@ -0,0 +1,40 @@
+% punch : calls punch (John Hole, eikonal)
+%
+% CALL : 
+%   t=punch(Vel,x,y,z,S)
+%
+function t=punch(Vel,x,y,z,S)
+  
+
+  punch_bin='~/RESEARCH/PROGRAMMING/GSLIB/visim/visim_examples/punch';
+  
+  fvel='punch.vel';
+  ftime='punch.time';
+  fpunch='punch.par';
+   
+  write_bin('punch.vel',Vel(:));
+  
+  dx=x(2)-x(1);
+  
+  nx=length(x);
+  ny=length(y);
+  nz=length(z);
+  
+  fxs=S(1);
+  fys=S(2);
+  fzs=S(3);
+  
+  x0=x(1);
+  y0=y(1);
+  z0=z(1);
+  
+  reverse=1;
+  maxoff=sqrt((max(x)-min(x)).^2+(max(y)-min(y)).^2+(max(z)-min(z)).^2);
+  
+  write_punch_par(fpunch,ftime,fvel,fxs,fys,fzs,nx,ny,nz,x0,y0,z0,dx,reverse,maxoff)
+  
+  unix(sprintf('%s par=%s',punch_bin,fpunch));
+  
+  t=read_bin(ftime,nx);
+  
+  
