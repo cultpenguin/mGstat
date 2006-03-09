07e25fc621a368273110b25313a7e5d41ac3cb1f
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2006-03-09 23:27:19 +0000

    *** empty log message ***

--- trunk/mGstat/MakeXmlRef.m
+++ trunk/mGstat/MakeXmlRef.m
@@ -1,19 +1,22 @@
-f=dir('*.m')
+f{1}=dir('*.m')
+f{2}=dir('visim/*.m')
 
 fid=fopen('mgstat_function.xml','w');
 
-for i=1:length(f)
-  [p,name,ext]=fileparts(f(i).name);
-  disp(name)
-  h=help(name);
-  
-  fprintf(fid,'<sect1 id=\"%s\"><title>%s</title>\n',name,name);
-  fprintf(fid,'<para><programlisting><![CDATA[%s]]></programlisting></para>\n',h);
-  fprintf(fid,'</sect1>\n\n');
-  
-  
-  
-end
+for ff=1:length(f)
+
+    for i=1:length(f{ff})
+      [p,name,ext]=fileparts(f{ff}(i).name);
+      disp(name)
+      h=help(name);
+      
+      fprintf(fid,'<sect1 id=\"%s\"><title>%s</title>\n',name,name);
+      fprintf(fid,'<para><programlisting><![CDATA[%s]]></programlisting></para>\n',h);
+      fprintf(fid,'</sect1>\n\n');
+      
+    end
+    
+  end
 
 fclose(fid);
   
