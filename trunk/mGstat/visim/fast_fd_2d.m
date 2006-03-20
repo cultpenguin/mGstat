0aa5cbc2639287a8fa97c9ed32a1868bcc8042aa
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2006-03-20 22:22:48 +0000

    *** empty log message ***

--- trunk/mGstat/visim/fast_fd_2d.m
+++ trunk/mGstat/visim/fast_fd_2d.m
@@ -1,22 +1,47 @@
 % fast_fd_2d : wrapper for the 'fd' eikonal solver from FAST
+%
+% CALL : 
+%   t=fast_fd_2d(x,z,V,Sources);
+%
+%  x,z :  [km]
+%  V   :  [m/s]
+%
+%
 function t=fast_fd_2d(x,z,V,Sources);
+
+  V_gain=1000;
+
+  V=V.*V_gain;
+  
+  fd_bin='/scratch/tmh/RESEARCH/PROGRAMMING/mGstat/bin/nfd';
+  if exist(fd_bin)==0,
+    disp(sprintf('%s - NO VALID PATH TO nfd',mfilename));
+  end
   
   % MAKE LOTYS OF TEST THAT V IS CORRECTLY SHAPED
   %
-  nx=length(x);
-  nz=length(z);
-  
-  fd_bin='~/bin/nfd';
-  
+
   % WRITE VELOCITY FILE
   write_bin('vel.mod',V,1,'int16');
 
  
-  % WRITE FASTY 'fd' parameter files  
-  o=fast_fd_write_par(Sources(:,1),Sources(:,2));
+  % WRITE FAST 'fd' parameter files  
+  o.xmin=min(x);
+  o.xmax=max(x);
+  o.ymin=0;
+  o.ymax=0;
+  o.zmin=min(z);
+  o.zmax=max(z);
+  o.dx=x(2)-x(1);
+  o.nx=length(x);
+  o.ny=1;
+  o.nz=length(z);
+  o.tmax=200;
+  o.tmax=V_gain.*( sqrt( (max(x)-min(x)).^2 +  (max(z)-min(z)).^2 )) ./ min(V(:));
+  o=fast_fd_write_par(Sources(:,1),Sources(:,2),o);
   
   % RUN 'fd'
   unix(fd_bin);
   
   % READ OUTPUT
-  t=read_bin('fd01.times',nx,nz,1,'uint16').*o.tmax./32766;
+  t=read_bin('fd01.times',o.nx,o.nz,1,'uint16').*o.tmax./32766;
