93f5036c70eff7681add326b439ef903b47b79ca
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2006-04-24 13:37:21 +0000

    *** empty log message ***

---
+++ visim/visim_calc_semivar_prob.m
@@ -0,0 +1,17 @@
+function [P,Pall]=visim_calc_semivar_prob(g_pdf,g_arr,hc,g)
+% [P,Pall]=visim_calc_semivar_prob(g_pdf,g_arr,hc,g)
+
+for i=1:length(hc)
+%  disp(sprintf('CENTER %4.2f',hc(i)))
+  
+  pdf=g_pdf(:,i)';
+  
+  Pall(i)=interp1(g_arr,pdf,g(i),'nearest');
+end
+P=sum(Pall)./length(hc);
+
+
+%P=comb_cprob_nd(.1,Pall(2:length(Pall)));
+
+
+%P=prod(Pall);
