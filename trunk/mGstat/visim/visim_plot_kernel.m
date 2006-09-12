ed6d5fef19e6d162db036220627e2f4e0fd3eac1
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2006-09-12 08:32:12 +0000

    *** empty log message ***

--- trunk/mGstat/visim/visim_plot_kernel.m
+++ trunk/mGstat/visim/visim_plot_kernel.m
@@ -12,26 +12,43 @@ function visim_plot_kernel(V,ivol,G,doPlot)
     V=read_visim(V);
   end
 
-  if nargin==1;
-    ivol=1:size(V.fvolsum.data,1);
-  end
-  
-  if nargin<3
-    G=visim_to_G(V);
-  end
+  if isfield(V,'fvolsum')
     
-  if length(ivol)==1
-    gg=G(ivol,:);
-  else
-    gg=sum(G(ivol,:));
+    if nargin==1;
+      ivol=1:size(V.fvolsum.data,1);
+    end
+    
+    if nargin<3
+      G=visim_to_G(V);
+    end
+    
+    if length(ivol)==1
+      gg=G(ivol,:);
+    else
+      gg=sum(G(ivol,:));
+    end
+    
+    
+    imagesc(V.x,V.y,reshape(gg,V.nx,V.ny)');
+    axis image
+    txt=title(sprintf('Kernel for  %s',V.parfile));
+    set(txt,'interpreter','none');
+  
+    hold on
+    
   end
   
+  if (isfield(V,'fconddata'))
+    d=V.fconddata.data;
+    scatter( d(:,V.cols(1)), d(:,V.cols(2)), 20 , d(:,V.cols(4)) )
+  end
   
-  imagesc(V.x,V.y,reshape(gg,V.nx,V.ny)');
+  hold off
+  
+  axis([V.x(1) max(V.x) V.y(1) max(V.y)])
   axis image
-  txt=title(sprintf('Kernel for  %s',V.parfile));
-  set(txt,'interpreter','none');
-
+  
+  
   if doPlot>0
     
     [f1,f2,f3]=fileparts(V.parfile);
