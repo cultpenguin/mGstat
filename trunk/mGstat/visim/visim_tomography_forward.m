f52b567000767ec11aa57a346ecd84116aafdcb0
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2007-01-08 10:46:54 +0000

    *** empty log message ***

---
+++ trunk/mGstat/visim/visim_tomography_forward.m
@@ -0,0 +1,16 @@
+% visim_tomography_forward(V,S,R,t,t_err,vref,options)
+function [V,t,t_err]=visim_tomography_forward(V,S,R,t,t_err,vref,options);
+	
+	if isfield(options,'linear_kernel')
+ 		v0=vref.*0+V.gmean;
+	else
+		v0=vref;	
+	end
+	
+
+	v0=vref.*0+V.gmean;
+
+	[V,G,Gr,rl]=visim_setup_tomo_kernel(V,S,R,v0,t,t_err,options.name,options.type,options.doPlot);
+	G=visim_to_G(V);
+	t=rl'./(Glin*vref(:));
+	[V]=visim_setup_tomo_kernel(V,S,R,v0,t,t_err,options.name,options.type,options.doPlot);
