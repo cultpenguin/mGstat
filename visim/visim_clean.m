b78d0dd3edb10e1035f2703dc435734dda493652
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2007-05-21 08:24:49 +0000

    *** empty log message ***

--- visim/visim_clean.m
+++ visim/visim_clean.m
@@ -3,9 +3,25 @@
 %
 function visim_clean(V)
     
-    delete(['*_',V.out.fname])
-    
+    if nargin==0
+        files=dir('*.par');
+        
+        for i=1:length(files)
+            V=read_visim(files(i).name);
+            delete(['*_',V.out.fname])            
+        end
+        
+    else
+        if ~struct(V)
+            V=read_visim(V);
+        end
+            
+        delete(['*_',V.out.fname])
+    end
 
+    delete('fort.59');
+    delete('fort.1');
+    
     
     
 
