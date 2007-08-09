bf1346c33528c62945787076559b2c7567cfe395
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2007-08-09 09:46:11 +0000

    *** empty log message ***

--- trunk/mGstat/visim/visim_format_variogram.m
+++ trunk/mGstat/visim/visim_format_variogram.m
@@ -3,20 +3,27 @@
 % Call : 
 %      [str1,str2]=visim_format_variogram(V,comp);
 %
-function [str1,str2]=visim_format_variogram(V,comp);
+function [str1,str2,str3]=visim_format_variogram(V,comp);
   
   if nargin<2
     comp=1;
   end
   
   Va=V.Va;
-  
-  str1='';  
-  str2='';  
+
+  if V.Va.nugget>0
+      str1=sprintf(' %8.3f Nug(0)',V.Va.nugget);
+      str2=str1;
+      str3=str1;
+  else  
+      str1='';  
+      str2='';  
+      str3='';  
+  end
   
   for i=1:Va.nst
 
-    if (i>1)
+      if ((i>1)|(V.Va.nugget>0))
         str1=[str1,' + '];
         str2=[str2,' + '];
     end        
@@ -32,9 +39,11 @@ function [str1,str2]=visim_format_variogram(V,comp);
     if comp==0,
       str1=sprintf('%s %8.3f %s(%5.1f)',str1,Va.cc(i),type,Va.a_hmax(i));
       str2=sprintf('%s %8.3f %s(%5.1f)',str2,Va.cc(i),type,Va.a_hmin(i));
+      str3=sprintf('%s %8.3f %s(%5.1f)',str3,Va.cc(i),type,Va.a_vert(i));
     else
       str1=sprintf('%s %6.4g %s(%5.3f)',str1,Va.cc(i),type,Va.a_hmax(i));
       str2=sprintf('%s %6.4g %s(%5.3f)',str2,Va.cc(i),type,Va.a_hmin(i));
+      str3=sprintf('%s %6.4g %s(%5.3f)',str3,Va.cc(i),type,Va.a_vert(i));
     end
     
   end
