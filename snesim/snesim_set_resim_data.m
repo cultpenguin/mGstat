ad587b1c2a3d9940f7e9a799f941f855aaa368b3
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2008-05-01 09:51:25 +0000

    *** empty log message ***

--- snesim/snesim_set_resim_data.m
+++ snesim/snesim_set_resim_data.m
@@ -1,4 +1,17 @@
-function snesim_set_resim_data(S,D,lim,pos)
+% snesim_set_resim_data : Select and set a region to resimulate.
+%
+% Call
+%    S=snesim_set_resim_data(S,D,lim,pos)
+%
+%    S : SNESIM of VISIM structure
+%    D : completete conditional data set (e.g. S.D(:,:,1));
+%    lim : lim(1) : horizontal radius (in cells)
+%          lim(2) : vertical radius (in cells)
+%
+%    pos : center point pos=[x,y] for pertubation
+%
+function S=snesim_set_resim_data(S,D,lim,pos)
+
 
 if nargin<2
 D=S.D(:,:,1)';
@@ -6,17 +19,14 @@ end
 
 
 if nargin<3
-lim(1)=20;
-lim(2)=5;
+lim(1)=3;
+lim(2)=3;
 end
-
 
 if nargin<4
 	pos(1)=min(S.x)+rand(1)*(max(S.x)-min(S.x));
 	pos(2)=min(S.y)+rand(1)*(max(S.y)-min(S.y));
 end
-
-
 
 
 
@@ -27,5 +37,10 @@ used(find( (abs(xx-pos(1))<lim(1)) & (abs(yy-pos(2))<lim(2)) ))=0;
 ih=find(used);
 
 d_cond=[xx(ih) yy(ih) yy(ih).*0+S.y(1) D(ih)];
+
+if isfield(S,'fconddata')==0
+    S.fconddata.fname='cond.eas';
+end
 write_eas(S.fconddata.fname,d_cond);
-    
+
+
