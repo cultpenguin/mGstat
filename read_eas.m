5a03a4bc77a0bec9c273e618c760503ec309f5b5
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2008-08-05 13:52:44 +0000

    *** empty log message ***

--- read_eas.m
+++ read_eas.m
@@ -4,10 +4,20 @@
 %
 % TMH (tmh@gfy.ku.dk)
 %
-function [data,header,txtdata,txtheader]=read_eas(filename);
+function [data,header,dim,txtdata,txtheader]=read_eas(filename);
    fid=fopen(filename,'r');
    txt_title=fgetl(fid);
-
+   try
+       id1=strfind(txt_title,'(');
+       id2=strfind(txt_title,')');
+       dims=txt_title( (id1+1):(id2-1) );
+       
+       j=findstr(dims,'x');
+       dim.nx=str2num(dims(1:(j(1)-1)));
+       dim.ny=str2num(dims((j(1)+1):(j(2)-1)));
+       dim.nz=str2num(dims((j(2)+1):length(dims)));
+   end   
+   
    %disp(txt_title);
    l=fgetl(fid);
    % next 3 lines to satisfy sisim.f
@@ -15,33 +25,24 @@ function [data,header,txtdata,txtheader]=read_eas(filename);
      l=fgetl(fid);
    end
    nc=str2num(l);
-      
    for i=1:nc,
      header{i}=fgetl(fid);
    end
    header=regexprep(header,char(9),'');
-
    nvar=length(header);
-   
    fpos_start=ftell(fid);
-   
    use_alt=0;
    txtdata=[];
    txtheader=[];
-   
    try
-          
      data=fscanf(fid,'%f',[inf]);
      ndata=length(data)./length(header);
      data=reshape(data(:),nvar,ndata)';
-     
    catch
      use_alt=1;
    end
-   
    % If no data has been read, use alternate method
    if isempty(data), use_alt=1; end
-   
    if use_alt==1,
      clear data
      disp('Trying to read data into cell structure')
@@ -51,14 +52,12 @@ function [data,header,txtdata,txtheader]=read_eas(filename);
        i=i+1;
        if (i/1000)==round(i/1000),disp(sprintf('%d',i));end
        s=fgetl(fid);
-       
        % Strip line into values seperated by space(s).
        l = {};
        while (length(s) > 0)
          [t,s] = strtok(s);
          l = {l{:}, t};
        end
-
 %       disp(i)
        txtdata_header=[];
        h1=0;h2=0;
@@ -68,7 +67,6 @@ function [data,header,txtdata,txtheader]=read_eas(filename);
            if (~isempty((str2num(val))))&(~strcmp(val,'i'))           
              val=str2num(val);
              data.(header{i2})(i)=val;
-             
              h1=h1+1;data_header{h1}=header{i2};
            else           
              if ~isempty(val)
@@ -76,20 +74,14 @@ function [data,header,txtdata,txtheader]=read_eas(filename);
                h2=h2+1;txtdata_header{h2}=header{i2};
              else
                % disp(sprintf('data line %d pos %d: Empty Value',i,i2))
-             end    
+             end
            end
          catch
-           disp('READ ERROR')
-
+             disp('READ ERROR')
          end
        end
-       
-%       if i==10000;
-%         return
-%       end
      end
      % convert data to normal eas matrix
-
      nh=length(data_header);
      nd=length(data.(header{1}));
      data_out=zeros(nd,nh);
@@ -100,14 +92,10 @@ function [data,header,txtdata,txtheader]=read_eas(filename);
      data=data_out;
      header=data_header;
      txtheader=txtdata_header;
-     
-     
-     
    end
    
    
    
    fclose(fid);
-
    
    
--- sgems/sgems_sgsim_test.py
+++ sgems/sgems_sgsim_test.py
@@ -7,11 +7,11 @@ sgems.execute('DeleteObjects SIM')
 sgems.execute('DeleteObjects finished')
 sgems.execute('NewCartesianGrid  SIM::100::100::1::1.0::1.0::1.0::0::0::0')
 
-for maxcd in range(1,10,1):
+for maxcd in range(1,5,1):
 
   property_name='SIM'+str(maxcd)
 
-  cmd='RunGeostatAlgorithm  sgsim::/GeostatParamUtils/XML::<parameters>  <algorithm name="sgsim" />     <Grid_Name  value="SIM"  />     <Property_Name  value="'+property_name+'" />     <Nb_Realizations  value="1" />     <Seed  value="14071789" />     <Kriging_Type  value="Simple Kriging (SK)"  />     <Trend  value="0 0 0 0 0 0 0 0 0 " />    <Local_Mean_Property  value=""  />     <Assign_Hard_Data  value="1"  />     <Hard_Data  grid=""   property=""  />     <Max_Conditioning_Data  value="'+str(maxcd)+'" />     <Search_Ellipsoid  value="80 80 80  0 0 0" />    <Use_Target_Histogram  value="0"  />     <nonParamCdf  ref_on_file ="0"  ref_on_grid ="1"  break_ties ="0" filename =""   grid =""  property ="">  <LTI_type  function ="Power"  extreme ="0"  omega ="3" />  <UTI_type  function ="Power"  extreme ="0"  omega ="0.333" />  </nonParamCdf>    <Variogram  nugget="0.001" structures_count="1"  >    <structure_1  contribution="0.999"  type="Gaussian"   >      <ranges max="'+str(j)+'"  medium="'+str(j/2)+'"  min="'+str(j/3)+'"   />      <angles x="'+str(i)+'"  y="0"  z="0"   />    </structure_1>  </Variogram>  </parameters>'   
+  cmd='RunGeostatAlgorithm  sgsim::/GeostatParamUtils/XML::<parameters>  <algorithm name="sgsim" />     <Grid_Name  value="SIM"  />     <Property_Name  value="'+property_name+'" />     <Nb_Realizations  value="2" />     <Seed  value="14071789" />     <Kriging_Type  value="Simple Kriging (SK)"  />     <Trend  value="0 0 0 0 0 0 0 0 0 " />    <Local_Mean_Property  value=""  />     <Assign_Hard_Data  value="1"  />     <Hard_Data  grid=""   property=""  />     <Max_Conditioning_Data  value="'+str(maxcd)+'" />     <Search_Ellipsoid  value="80 80 80  0 0 0" />    <Use_Target_Histogram  value="0"  />     <nonParamCdf  ref_on_file ="0"  ref_on_grid ="1"  break_ties ="0" filename =""   grid =""  property ="">  <LTI_type  function ="Power"  extreme ="0"  omega ="3" />  <UTI_type  function ="Power"  extreme ="0"  omega ="0.333" />  </nonParamCdf>    <Variogram  nugget="0.001" structures_count="1"  >    <structure_1  contribution="0.999"  type="Gaussian"   >      <ranges max="'+str(j)+'"  medium="'+str(j/2)+'"  min="'+str(j/3)+'"   />      <angles x="'+str(i)+'"  y="0"  z="0"   />    </structure_1>  </Variogram>  </parameters>'   
 
   sgems.execute(cmd)
   sgems.execute('DisplayObject SIM::'+property_name+'__real0')
