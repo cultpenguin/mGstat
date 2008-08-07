f56695b43c38cfba47682699330bd7ef08dcc5bb
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2008-08-07 13:07:04 +0000

    *** empty log message ***

--- trunk/mGstat/examples/manuscripts/FrykmanDeutsch2002/ScalingLawDemo.cmd
+++
@@ -1,8 +0,0 @@
-data(test): dummy, sk_mean=25, max=100;
-data(): 'ScalingLawDemo.eas', x=1;
-variogram(test):   3.60000000 Sph(0.54);
-set output = 'ScalingLawDemo.out.out';
-set mv = 'NaN';
-set format = '%12.8f';
-set seed = 12;
-method: gs;
