94ec544ef4347829ef2beddb0ced9c4edd2eb4f6
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2008-09-09 09:42:55 +0000

    *** empty log message ***

--- trunk/mGstat/sgems/sgems_ppm.m
+++ trunk/mGstat/sgems/sgems_ppm.m
@@ -1,12 +1,16 @@
-% sgems_ppm : Probability pertubation
+% sgems_ppm : Probability perturbation
 %
 % Example : 
-%   S=sgems_get_par('snesim_std');
-%   S.XML.parameters.Nb_Realizations.value=1;
-%   S=sgems_grid(S);
-%   S_1=sgems_ppm(S,.1);
-%   S_2=sgems_ppm(S,.5);
-%   S_3=sgems_ppm(S,1);
+%        S=sgems_get_par('snesim_std');
+%        S.XML.parameters.Nb_Realizations.value=1;
+%        S=sgems_grid(S);
+%        r_arr=linspace(0.1,1,25);
+%        for i=1:length(r_arr)
+%            Sppm{i}=sgems_ppm(S,S.O,r_arr(i));
+%            subplot(5,5,i);
+%            imagesc(S.x,S.y,Sppm{i}.D');axis image;
+%            title(r_arr(i))
+%        end
 function S=sgems_ppm(S,O,r);
 
 if nargin==0
@@ -18,10 +22,10 @@ if nargin==0
         S=sgems_get_par('snesim_std');
         S.XML.parameters.Nb_Realizations.value=1;
         S=sgems_grid(S);
-        r_arr=[.1 .5];
+        r_arr=linspace(0.1,1,25);
         for i=1:length(r_arr)
             Sppm{i}=sgems_ppm(S,S.O,r_arr(i));
-            subplot(2,2,i);
+            subplot(5,5,i);
             imagesc(S.x,S.y,Sppm{i}.D');axis image;
             title(r_arr(i))
         end
@@ -36,18 +40,22 @@ end
 O.grid_name='PROB';
 S.XML.parameters.Seed.value=S.XML.parameters.Seed.value+1;
 tau=r;
+data=O.data(1,:);
 for ig=1:S.XML.parameters.Nb_Facies.value
     O.property{ig}=sprintf('F%d',ig-1);
         
     marg_pdf=S.XML.parameters.Marginal_Cdf.value(ig);
-    
-    d=(1-tau).*(O.data(1,:)==(ig-1))+tau.*marg_pdf;
+    d=data.*0;
+    ii{ig}=find(data==(ig-1));
+    d(ii{ig})=1;
+    p_facies=(1-tau).*d+tau.*marg_pdf;
+    %p_facies=(1-tau).*[O.data(1,:)==(ig-1)]+tau.*marg_pdf
+    %keyboard
+    O.data(ig,:)=p_facies;
     O.n_prop=length(O.property{ig}); % update
-    O.data(ig,:)=d;
 end
 S.f_probfield=sprintf('prob_%d.sgems',round(tau*100));
 sgems_write(S.f_probfield,O);
-
 S=sgems_grid(S);
 
 
