33c02ed00e010b613cfcacdf1706a5825c2b52bb
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2008-10-03 11:18:51 +0000

    *** empty log message ***

--- trunk/mGstat/sgems/sgems_ppm.m
+++ trunk/mGstat/sgems/sgems_ppm.m
@@ -17,18 +17,10 @@ if nargin==0
     
     help(mfilename)
     
-    str=input('Do you want to see an example of PPMM ? [''Y''/N] ','s');
+    str=input('Do you want to see an example of PPM ? [''Y''/N] ','s');
+
     if strcmp(upper(str),'Y')            
-        S=sgems_get_par('snesim_std');
-        S.XML.parameters.Nb_Realizations.value=1;
-        S=sgems_grid(S);
-        r_arr=linspace(0.1,1,25);
-        for i=1:length(r_arr)
-            Sppm{i}=sgems_ppm(S,S.O,r_arr(i));
-            subplot(5,5,i);
-            imagesc(S.x,S.y,Sppm{i}.D');axis image;
-            title(r_arr(i))
-        end
+        sgems_example_ppm
     end
     return
 end
@@ -37,11 +29,13 @@ if isstr(O)
     O=sgems_read(O);
 end
 
+
 O.grid_name='PROB';
 S.XML.parameters.Seed.value=S.XML.parameters.Seed.value+1;
 tau=r;
-data=O.data(1,:);
+data=O.data(:,1);
 for ig=1:S.XML.parameters.Nb_Facies.value
+
     O.property{ig}=sprintf('F%d',ig-1);
         
     marg_pdf=S.XML.parameters.Marginal_Cdf.value(ig);
@@ -50,12 +44,13 @@ for ig=1:S.XML.parameters.Nb_Facies.value
     d(ii{ig})=1;
     p_facies=(1-tau).*d+tau.*marg_pdf;
     %p_facies=(1-tau).*[O.data(1,:)==(ig-1)]+tau.*marg_pdf
-    %keyboard
-    O.data(ig,:)=p_facies;
+    O.data(:,ig)=p_facies;
     O.n_prop=length(O.property{ig}); % update
 end
 S.f_probfield=sprintf('prob_%d.sgems',round(tau*100));
+
 sgems_write(S.f_probfield,O);
+
 S=sgems_grid(S);
 
 
