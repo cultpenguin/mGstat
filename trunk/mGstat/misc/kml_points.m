9878b000cb0efb7b6924af7aea43d2912755cb05
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2009-02-09 15:43:02 +0000

    *** empty log message ***

--- trunk/mGstat/misc/kml_points.m
+++ trunk/mGstat/misc/kml_points.m
@@ -25,20 +25,57 @@ fprintf(fid,'<?xml version="1.0" encoding="UTF-8"?>\n');
 fprintf(fid,'<kml xmlns="http://www.opengis.net/kml/2.2">\n');
 if (length(lat)>1)
     fprintf(fid,'<Folder>');
-    fprintf(fid,'      <name>%s</name>',name);
-    fprintf(fid,'      <description>%s</description>',descr);
+    try
+        fprintf(fid,'      <name>%s</name>',name);
+        fprintf(fid,'      <description>%s</description>',descr);
+    end
 end    
-fprintf(fid,'<Style id="triangle"><IconStyle><Icon><Scale>0.1</Scale><href>http://maps.google.com/mapfiles/kml/pal4/icon52.png</href></Icon></IconStyle></Style>');
-fprintf(fid,'<Style id="shot"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/pal4/icon51.png</href></Icon></IconStyle></Style>');
-fprintf(fid,'<Style id="arrowdown"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/pal4/icon28.png</href></Icon></IconStyle></Style>');
-fprintf(fid,'<Style id="globe"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/pal3/icon19.png</href></Icon></IconStyle></Style>');
+fprintf(fid,'<Style id="triangle"><IconStyle><Icon><Scale>0.1</Scale><href>http://maps.google.com/mapfiles/kml/pal4/icon52.png</href></Icon></IconStyle></Style>\n');
+fprintf(fid,'<Style id="shot"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/pal4/icon51.png</href></Icon></IconStyle></Style>\n');
+fprintf(fid,'<Style id="arrowdown"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/pal4/icon28.png</href></Icon></IconStyle></Style>\n');
+fprintf(fid,'<Style id="globe"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/pal3/icon19.png</href></Icon></IconStyle></Style>\n');
+if isnumeric(icon)
+    for i=1:length(icon)
+    fprintf(fid,'<Style id="%d"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/pal3/icon%d.png</href></Icon></IconStyle></Style>\n',icon(i),icon(i));
+    end
+end
+
 for i=1:length(lat);
 fprintf(fid,'  <Placemark>\n');
-name=sprintf('%d',i);
-fprintf(fid,'    <name>%s</name>\n',name);
-fprintf(fid,'    <description>%s\n',descr);
+name_str='';
+if iscell(name);
+    try
+        name_str=name{i};
+    end
+else
+    try
+       name_str=name;
+    end
+end
+
+descr_str='';
+if iscell(descr);
+    try
+        descr_str=descr{i};
+    end
+else
+    try
+       descr_str=descr;
+    end
+end
+
+fprintf(fid,'    <name>%s</name>\n',name_str);
+fprintf(fid,'    <description>%s\n',descr_str);
 fprintf(fid,'    </description>\n');
-fprintf(fid,'    <styleUrl>#%s</styleUrl>\n',icon);
+if isnumeric(icon)
+    try
+        fprintf(fid,'    <styleUrl>#%d</styleUrl>\n',icon(i));
+    catch
+        fprintf(fid,'    <styleUrl>#%d</styleUrl>\n',icon);
+    end
+else
+    fprintf(fid,'    <styleUrl>#%s</styleUrl>\n',icon);
+end
 fprintf(fid,'    <Point>\n');
 fprintf(fid,'      <coordinates>%16.6f,%16.6f,%g</coordinates>\n',lon(i),lat(i),ele(i));
 fprintf(fid,'    </Point>\n');
