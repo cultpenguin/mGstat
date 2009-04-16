f50f2dca925c368531859b9610a7fcba94959c6d
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2009-04-16 06:42:28 +0000

    Changed default dimension of empty dimensions from one to zero.

--- trunk/mGstat/sgems/sgems_grid.m
+++ trunk/mGstat/sgems/sgems_grid.m
@@ -18,7 +18,7 @@ if isfield(S.dim,'x');
     try
         S.dim.dx=S.dim.x(2)-S.dim.x(1);
     catch
-        S.dim.dx=1;
+        S.dim.dx=0;
     end
 end
 if isfield(S.dim,'y');
@@ -28,7 +28,7 @@ if isfield(S.dim,'y');
     try
         S.dim.dy=S.dim.y(2)-S.dim.y(1);
     catch
-        S.dim.dy=1;
+        S.dim.dy=0;
     end
 end
 if isfield(S.dim,'z');
@@ -37,7 +37,7 @@ if isfield(S.dim,'z');
     try
         S.dim.dz=S.dim.z(2)-S.dim.z(1);
     catch
-        S.dim.dz=1;
+        S.dim.dz=0;
     end
 end
 
