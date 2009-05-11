bea2487bf2c6e68a3c3c57b4c0a872cbc3008f28
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2009-05-11 11:41:09 +0000

    Fixed bug in precal_cov (covariance matrix was wrong in case of a nugget in the variogram model)

--- krig_optim_mcmc.m
+++ krig_optim_mcmc.m
@@ -1,5 +1,5 @@
 % krig_optim_mcmc
-% CALL : 
+% CALL :
 %   [V_new,be_acc,L_acc,par2,nugfrac_acc,V_acc,options]=krig_optim_mcmc(pos_known,val_known,V,options)
 %
 function [V_new,be_acc,L_acc,par2,nugfrac_acc,V_acc,options]=krig_optim_mcmc(pos_known,val_known,V,options);
@@ -7,61 +7,61 @@ function [V_new,be_acc,L_acc,par2,nugfrac_acc,V_acc,options]=krig_optim_mcmc(pos
 V_new=V;
 
 if isstr(V),
-  V=deformat_variogram(V);
-end 
+    V=deformat_variogram(V);
+end
 
 options.isorange=1;
 
 if isfield(options,'max_range')
-  max_range=options.max_range;
+    max_range=options.max_range;
 else
-  max_range=10*std(pos_known);
+    max_range=10*std(pos_known);
 end
 
 if isfield(options,'step_range')
-  step_range=options.step_range;
+    step_range=options.step_range;
 else
     step_range=std(pos_known)/112;
 end
 
 if isfield(options,'step_nugfrac')
-  step_nugfrac=options.step_nugfrac;
+    step_nugfrac=options.step_nugfrac;
 else
-  step_nugfrac=.1;
+    step_nugfrac=.1;
 end
 
 if isfield(options,'annealing')
-  annealing=options.annealing;
+    annealing=options.annealing;
 else
-  annealing=0;
+    annealing=0;
 end
 
 if isfield(options,'descent')
-  descent=options.descent;
+    descent=options.descent;
 else
-  descent=0;
+    descent=0;
 end
 
 if isfield(options,'gvar');
-  gvar=options.gvar;
+    gvar=options.gvar;
 else
-  gvar=var(val_known);
+    gvar=var(val_known);
 end
 
 
 if isfield(options,'maxit');
-  maxit=options.maxit;
+    maxit=options.maxit;
 else
-  maxit=100;
+    maxit=100;
 end
 
 
 if isfield(options,'method');
-  method=options.method;
+    method=options.method;
 else
-  method=1;
-  % 1: Maximum Likelihood
-  % 2: Maximum likelihood cross validation
+    method=1;
+    % 1: Maximum Likelihood
+    % 2: Maximum likelihood cross validation
 end
 
 ndim=size(pos_known,2);
@@ -75,12 +75,12 @@ std_known=std(pos_known);
 mean_known=mean(pos_known);
 
 
-% A PRIORI 
+% A PRIORI
 na=25;
 for idim=1:ndim
-  narr{idim}=na;
-  arr{idim}=linspace(0,2*std_known(idim),narr{idim});
-  arr{idim}(1)=0.01;
+    narr{idim}=na;
+    arr{idim}=linspace(0,2*std_known(idim),narr{idim});
+    arr{idim}(1)=0.01;
 end
 
 V_init=V;
@@ -92,12 +92,12 @@ if method==1
     L_init=krig_covar_lik(pos_known,val_known,V,options);
     be_init=0;
 else
-    [d_est,d_var,be_init,d_diff,L_init]=krig_blinderror(pos_known,val_known,pos_known,V_init,options);    
+    [d_est,d_var,be_init,d_diff,L_init]=krig_blinderror(pos_known,val_known,pos_known,V_init,options);
 end
 
 if (isinf(L_init))
-  L_init=1e-300;
-end	
+    L_init=1e-300;
+end
 
 if L_init==0
     L_init=1e-300;
@@ -117,183 +117,187 @@ L_all=zeros(1,maxit);
 %be_all=zeros(1,maxit);
 nugfrac_all=zeros(1,maxit);
 
-
+t_old_plot=now;
 
 nacc=0;
 i=0;icum=0;
 while i<=maxit
-  i=i+1;
-  icum=icum+1;
-  % Simulated Annealing
-  if annealing==1,
-    T=exp(-(i-1)/1000);
-    options.T=T;
-  end
-
-  % PERTURB MODEL
-  V_new = V_old;
-
-  % PERTURB RANGE
-  V_new(2).par2=V_new(2).par2 + randn(size(step_range)).*step_range;
-
-  % PERTURB NUGGET FRACTION
-  nugfrac=V_new(1).par1./gvar;
-  nugfrac=nugfrac+randn(1).*step_nugfrac;
-  V_new(1).par1=gvar.*nugfrac;
-  V_new(2).par1=gvar.*(1-nugfrac);
-  
-  % TEST FOR BOUNDS 
-  compL=1;
-  if ~isempty(find(V_new(2).par2<=0)), compL=0; end 
-  for idim=1:ndim
-    if ~isempty(find(V_new(2).par2(idim)>=max_range(idim))), 
-      compL=0;
+    i=i+1;
+    icum=icum+1;
+    % Simulated Annealing
+    if annealing==1,
+        T=exp(-(i-1)/1000);
+        options.T=T;
     end
-  end
 
-  if ((nugfrac<0)|(nugfrac>1))
-    compL=0;
-  end
- 
-  if compL==1
-    try
-      if method==1,
-          L_new=krig_covar_lik(pos_known,val_known,V_new,options);
-          be_new=0;
-      else
-          [d1,d2,be_new,d_diff,L_new]=krig_blinderror(pos_known,val_known,pos_known,V_new,options);
-      end
+    % PERTURB MODEL
+    V_new = V_old;
 
-    catch
-      %keyboard
-    end
-    
-  else
-      i=i-1; % THIS IS NOT A PARAMETER CHOICE TO BE CONSIDERED
-    %L_new=-1e-45;
-  end
-  
-  try
-  par2_all(i,:)=V_new(2).par2;  
-  catch
-  keyboard
-  end
-  L_all(i) = L_new;
-  be_all(i) = be_new;
-  nugfrac_all(i) = nugfrac;
-  
-  
-  % When L is likelihood
-  % Pacc=min([(L_new)/(L_old),1]);
-  % When L is LOG likelihood
-  Pacc=min([exp(L_new-L_old),1]);
+    % PERTURB RANGE
+    V_new(2).par2=V_new(2).par2 + randn(size(step_range)).*step_range;
 
-  
-  if compL==0
-    Pacc=0;
-  end
-  
-  if descent==1
-    % ONLY ACCEPT IMPROVEMENETS
-    Prand=1;
-  else
-    Prand=rand(1);
-  end
-  
-  if Pacc>=Prand
-    %  if Pacc==1  % ONLY ACCPET IMPROVEMENTS
-    
-    V_old=V_new;
-    L_old=L_new;
-    be_old=be_new;
-    
-    nacc=nacc+1;
-    
-    par2(nacc,:)=V_new(2).par2;
-    
-    L_acc(nacc) = L_new;
-    be_acc(nacc) = be_new;
-    V_acc{nacc} = V_new; 
-    nugfrac_acc(nacc) = nugfrac; 
-    
-    doPlot=1;
-    if ((doPlot==1)&(nacc>=1));
-      subplot(2,1,1)
-      plotyy(1:nacc,L_acc,1:nacc,-be_acc);
-      
-      if size(par2,2)==1
-        subplot(2,3,4)
-        %plot(par2(:,1),L_acc,'k.')
-        %[ax,h1,h2]=plotyy(par2(:,1),L_acc,par2(:,1),-be_acc);
-        [ax,h1,h2]=plotyy(L_acc,par2(:,1),L_acc,nugfrac_acc);
-        %[ax,h1,h2]=plotyy(L_all,par2_all(:,1),L_all,nugfrac_all);
-        set(h1,'LineStyle','none')
-        set(h2,'LineStyle','none')
-        set(h1,'Marker','.')
-        set(h2,'Marker','.')
-        set(h1,'color','b')
-        set(h2,'color','g')
-        set(get(ax(1),'Ylabel'),'String','Range')
-        set(get(ax(2),'Ylabel'),'String','NuggetFraction')
-        xlabel('L');
-        
-        subplot(2,3,5)
-        %scatter(par2(:,1),nugfrac_acc,20,L_acc,'filled')
-        %keyboard
-        scatter(par2_all(1:i,1),nugfrac_all(1:i),20,L_all(1:i),'filled')
-        xlabel('Range');ylabel('Nugget Fraction');title('L')
-        subplot(2,3,6)
-        if length(nugfrac_acc)>10
-            scatter(par2(:,1),nugfrac_acc,20,-be_acc,'filled')
+    % PERTURB NUGGET FRACTION
+    nugfrac=V_new(1).par1./gvar;
+    nugfrac=nugfrac+randn(1).*step_nugfrac;
+    V_new(1).par1=gvar.*nugfrac;
+    V_new(2).par1=gvar.*(1-nugfrac);
+
+    % TEST FOR BOUNDS
+    compL=1;
+    if ~isempty(find(V_new(2).par2<=0)), compL=0; end
+    for idim=1:ndim
+        if ~isempty(find(V_new(2).par2(idim)>=max_range(idim))),
+            compL=0;
         end
-        colorbar
-        xlabel('Range');ylabel('Nugget Fraction');title('BE')
+    end
 
-        drawnow;
-      elseif size(par2,2)==2
-        subplot(2,3,4)
-        scatter(par2(:,1),par2(:,2),22,L_acc,'filled')
-        xlabel('Range 1');ylabel('Range 2');title('Likelihood')
-        %colorbar
-        subplot(2,3,5)
-        scatter(par2(:,1),par2(:,2),22,-be_acc,'filled')
-        xlabel('Range 1');ylabel('Range 2');title('-be')
-        %colorbar
-        subplot(2,3,6)
-        scatter3(par2(:,1),par2(:,2),nugfrac_acc,20,L_acc,'filled');
-        xlabel('Range 1');ylabel('Range 2');zlabel('Nugget Fraction');title('Likelihood')
-        drawnow;
-      elseif size(par2,2)>2
-        subplot(2,1,2)
+    if ((nugfrac<0)|(nugfrac>1))
+        compL=0;
+    end
+
+    if compL==1
         try
-            [ax,h1,h2]=plotyy(1:1:nacc,par2,1:1:nacc,nugfrac_acc);
-        catch
-            keyboard
-        end
-        set(h1,'LineStyle','-','LineWidth',1)
-        set(h2,'LineStyle','-','LineWidth',2)
-        % set(h1,'Marker','.')
-        % set(h2,'Marker','.')
-        set(ax(1),'YScale','log')
-        set(h2,'color','k')
-        set(get(ax(1),'Ylabel'),'String','Range')
-        set(get(ax(2),'Ylabel'),'String','NuggetFraction')
-        xlabel('iteration');
-        legend(num2str([1:1:size(pos_known,2)]))
-        drawnow;
-      end
-    end
-    V_old=V_new;
-    L_old=L_new;
-    disp(sprintf('%3d/%4d --OK-- L = %6.3g  , PA=%4.2g Prand=%4.2g : %s',i,maxit,L_new,Pacc,Prand,format_variogram(V_new)))
-    %disp(sprintf('nugfrac=%5.4g  Accept rate = %4.2f%%',nugfrac,100.*nacc./i))
-  else
-      if compL==1;      
-        disp(sprintf('%3d/%4d ------ L = %6.3g  , PA=%4.2g Prand=%4.2g : %s',i,maxit,L_new,Pacc,Prand,format_variogram(V_new)))
-      end
-end
+            if method==1,
+                L_new=krig_covar_lik(pos_known,val_known,V_new,options);
+                be_new=0;
+            else
+                [d1,d2,be_new,d_diff,L_new]=krig_blinderror(pos_known,val_known,pos_known,V_new,options);
+            end
 
-  
+        catch
+            %keyboard
+        end
+
+        par2_all(i,:)=V_new(2).par2;
+        L_all(i) = L_new;
+        be_all(i) = be_new;
+        nugfrac_all(i) = nugfrac;
+
+    else
+        i=i-1; % THIS IS NOT A PARAMETER CHOICE TO BE CONSIDERED
+        %L_new=-1e-45;
+    end
+
+
+
+    % When L is likelihood
+    % Pacc=min([(L_new)/(L_old),1]);
+    % When L is LOG likelihood
+    Pacc=min([exp(L_new-L_old),1]);
+
+
+    if compL==0
+        Pacc=0;
+    end
+
+    if descent==1
+        % ONLY ACCEPT IMPROVEMENETS
+        Prand=1;
+    else
+        Prand=rand(1);
+    end
+
+    if Pacc>=Prand
+        %  if Pacc==1  % ONLY ACCPET IMPROVEMENTS
+
+        V_old=V_new;
+        L_old=L_new;
+        be_old=be_new;
+
+        nacc=nacc+1;
+
+        par2(nacc,:)=V_new(2).par2;
+
+        L_acc(nacc) = L_new;
+        be_acc(nacc) = be_new;
+        V_acc{nacc} = V_new;
+        nugfrac_acc(nacc) = nugfrac;
+
+        doPlot=1;
+        dt=(now-t_old_plot)*(3600*24);
+        if ((doPlot==1)&(nacc>=1)&(dt>5));
+            t_old_plot=now;
+            subplot(2,1,1)
+            plotyy(1:nacc,L_acc,1:nacc,-be_acc);
+
+            if size(par2,2)==1
+                % ONLY PLOT NMAX DATA
+                nn=size(par2,1);nmax=400;ndd=ceil(nn/nmax);ii=[ndd:ndd:nn];
+
+                subplot(2,3,4)
+                %plot(par2(:,1),L_acc,'k.')
+                %[ax,h1,h2]=plotyy(par2(:,1),L_acc,par2(:,1),-be_acc);
+                %[ax,h1,h2]=plotyy(L_acc,par2(:,1),L_acc,nugfrac_acc);
+                [ax,h1,h2]=plotyy(exp(L_acc(ii)),par2(ii,1),exp(L_acc(ii)),nugfrac_acc(ii));
+                %[ax,h1,h2]=plotyy(L_all,par2_all(:,1),L_all,nugfrac_all);
+                set(h1,'LineStyle','none')
+                set(h2,'LineStyle','none')
+                set(h1,'Marker','.')
+                set(h2,'Marker','.')
+                set(h1,'color','b')
+                set(h2,'color','g')
+                set(get(ax(1),'Ylabel'),'String','Range')
+                set(get(ax(2),'Ylabel'),'String','NuggetFraction')
+                xlabel('L');
+
+                subplot(2,3,5)
+                scatter(par2(ii,1),nugfrac_acc(ii),20,exp(L_acc(ii)),'filled')
+                %scatter(par2(:,1),nugfrac_acc,20,exp(L_acc),'filled')
+                %keyboard
+                %scatter(par2_all(1:i,1),nugfrac_all(1:i),20,L_all(1:i),'filled')
+                xlabel('Range');ylabel('Nugget Fraction');title('L')
+                subplot(2,3,6)
+                %if length(nugfrac_acc)>10
+                %    scatter(par2(:,1),nugfrac_acc,20,-be_acc,'filled')
+                %end
+                %colorbar
+                %xlabel('Range');ylabel('Nugget Fraction');title('BE')
+
+                drawnow;
+            elseif size(par2,2)==2
+                subplot(2,3,4)
+                scatter(par2(:,1),par2(:,2),22,L_acc,'filled')
+                xlabel('Range 1');ylabel('Range 2');title('Likelihood')
+                %colorbar
+                subplot(2,3,5)
+                scatter(par2(:,1),par2(:,2),22,-be_acc,'filled')
+                xlabel('Range 1');ylabel('Range 2');title('-be')
+                %colorbar
+                subplot(2,3,6)
+                scatter3(par2(:,1),par2(:,2),nugfrac_acc,20,L_acc,'filled');
+                xlabel('Range 1');ylabel('Range 2');zlabel('Nugget Fraction');title('Likelihood')
+                drawnow;
+            elseif size(par2,2)>2
+                subplot(2,1,2)
+                try
+                    [ax,h1,h2]=plotyy(1:1:nacc,par2,1:1:nacc,nugfrac_acc);
+                catch
+                    keyboard
+                end
+                set(h1,'LineStyle','-','LineWidth',1)
+                set(h2,'LineStyle','-','LineWidth',2)
+                % set(h1,'Marker','.')
+                % set(h2,'Marker','.')
+                set(ax(1),'YScale','log')
+                set(h2,'color','k')
+                set(get(ax(1),'Ylabel'),'String','Range')
+                set(get(ax(2),'Ylabel'),'String','NuggetFraction')
+                xlabel('iteration');
+                legend(num2str([1:1:size(pos_known,2)]))
+                drawnow;
+            end
+        end
+        V_old=V_new;
+        L_old=L_new;
+        disp(sprintf('%3d/%4d --OK-- L = %6.3g  , PA=%4.2g Prand=%4.2g : %s',i,maxit,L_new,Pacc,Prand,format_variogram(V_new)))
+        %disp(sprintf('nugfrac=%5.4g  Accept rate = %4.2f%%',nugfrac,100.*nacc./i))
+    else
+        %if compL==1;
+        %  disp(sprintf('%3d/%4d ------ L = %6.3g  , PA=%4.2g Prand=%4.2g : %s',i,maxit,L_new,Pacc,Prand,format_variogram(V_new)))
+        %end
+    end
+
+
 end
 
 % FIND BEST VARIOGRAM MODEL
--- precal_cov.m
+++ precal_cov.m
@@ -17,7 +17,6 @@
 %
 
 function [cov,d]=precal_cov(pos1,pos2,V,options);
-
 options.dummy='';
 if ~isfield(options,'verbose'), options.verbose=0;end
 
@@ -49,11 +48,11 @@ for iv=1:length(V)
     if length(par2)>1
         y_scale=par2(2);
         V(iv).par2=par2(1);
-        try
-            if par2(3)~=0
-                mgstat_verbose(sprintf('Rotation not supported, setting rot=0'),mfilename,10)
-            end
-        end
+%        try
+%            if par2(3)~=0
+%                mgstat_verbose(sprintf('Rotation not supported, setting rot=0'),mfilename,-1)
+%            end
+%        end
     else
         y_scale=1;
     end
@@ -63,7 +62,45 @@ try
     pos1(:,2)=pos1(:,2)./y_scale;
     pos2(:,2)=pos2(:,2)./y_scale;
 end
+tic;
+t=toc;
+semiv=zeros(size(d));
+for iV=1:length(V)
+    mgstat_verbose(sprintf('%s : semivar struc #%d',mfilename,iV),2)
 
+    for i=1:n_est1;
+        % progress bar
+        if ((t>0)&(options.verbose>0))
+            di=100;
+            if (i/di)==round(i/di)
+                progress_txt([i iV],[n_est1 length(V)],sprintf('%s : ',mfilename),'Nested struture');
+            end
+        end
+
+        % FAST VECTORIZED APPROACH
+        jj=1:n_est2;;            
+        if n_dim1==1
+            p1=ones(n_est1,1).*pos1(i);
+            p2=pos2;
+        else
+            p1=repmat(pos1(i,:),length(jj),1)';
+            p2=pos2(jj,:)';
+        end
+        dd=edist(p1,p2,V(iV).par2,isorange);
+        d(i,:)=dd;
+       
+        % SLOW ITERATIVE APPROACH
+        %for j=1:n_est2;
+        %    d(i,j)=edist(pos1(i,:),pos2(j,:),V(iV).par2,isorange);
+        %end
+    end
+
+    
+    semiv = semiv + semivar_synth(V(iV),d,1);      
+end
+cov=gvar-semiv;
+
+return
 tic
 
 for iV=1:length(V)
