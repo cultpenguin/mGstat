3c5fd7479ed502882df765dfe5827ceea4340475
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2009-06-02 12:03:30 +0000

    Added re-simulation through uncertain data. see visim_set_resim

--- trunk/mGstat/examples/mgstat_examples/perturbed_gdm_example.m
+++ trunk/mGstat/examples/mgstat_examples/perturbed_gdm_example.m
@@ -1,22 +1,30 @@
 % perutrbed_gdm_example : Comnbination of perturbed simualtion and GDM
 %
-% See also snesim_set_resim_data, grad_deform
+% See also visim_set_resim_data, grad_deform
 %
 doPlot=0;
 
+if ~exist('doResim'), doResim=1;end
+if ~exist('perturb_width'), perturb_width=5;end
+if ~exist('perturb_unc'), perturb_unc=10;end
+if ~exist('doGradDef'), doGradDef=0;end
+if ~exist('gdm_step'), gdm_step=0.6;end
+if ~exist('maxit');maxit=10000;end
+if ~exist('nsaves');nsaves=300;end
+if ~exist('dx','var');dx=2;end
+
+if nsaves>maxit
+    nsaves=maxit;
+end
+
 %doResim=1;doGradDef=1;gdm_step=0.6;perturb_width=10;
 %doResim=0;perturb_width=5;doGradDef=1;gdm_step=0.6;
-doResim=1;perturb_width=5;doGradDef=0;gdm_step=0.6;
-if ~exist('dx','var')
-    dx=2;
-end
+%doResim=1;perturb_width=5;doGradDef=0;gdm_step=0.6;
 V=visim_init([.5:dx:40],[.5:dx:40]);
 
 V.debuglevel=-2;
 V=visim(V);
 D=V.D;
-maxit=10000;
-nsaves=300;
 ij=round(linspace(1,maxit,nsaves));
 
 txt=sprintf('RSIM%d_RSIMWIDTH_%g_GDM%d_GDMSTEP_%g_dx%g',doResim,perturb_width,doGradDef,gdm_step,dx);
@@ -28,12 +36,16 @@ Dvar=zeros(1,maxit).*NaN;
 
 j=0;
 for i=1:maxit;
-    progress_txt(i,maxit,txt)
+    %progress_txt(i,maxit,txt)
     V.rseed=V.rseed+1;
     Vold=V;
     if doResim==1
-        V.cond_sim=2;
-        V=visim(snesim_set_resim_data(V,D',perturb_width.*[1 1]));
+        if perturb_unc>0
+            V.cond_sim=1;
+        else
+            V.cond_sim=2;
+        end
+        V=visim(visim_set_resim_data(V,D',perturb_width.*[1 1],[],perturb_unc));
     else
         V.cond_sim=0;
         V=visim(V);
@@ -85,8 +97,10 @@ for j=1:nsaves
 end
 Vpert=V;
 Vpert.D=Dsim;
-[g_uncon,hc_uncon]=visim_plot_semivar_real(V,[45 135],[10 10],[20 20],[1 1].*dx,0);
-[g_pert,hc_pert]=visim_plot_semivar_real(Vpert,[45 135],[10 10],[20 20],[1 1].*dx,0);
+
+%figure(2);
+%for i=1:Vpert.nsim;imagesc(V.x,V.y,Vpert.D(:,:,i));axis image;caxis([8 12]);drawnow;end
+%return;
 
 figure(2);set_paper;;clf;
 subplot(2,2,1);hist([mean_pert;mean_uncon]');legend('Perturbed','Uncon');xlabel('mean')
@@ -104,14 +118,6 @@ hold off
 s=suptitle(['PERTURBED ',txt]);set(s,'interpreter','none')
 print_mul(sprintf('%s_mean_var',txt))
 
-figure(3);;set_paper;;clf;
-for i=1:2;
-subplot(1,2,i);
-plot(hc_pert{i},g_pert{i},'k-',hc_uncon{i},g_uncon{i},'r-');
-end
-s=suptitle(['PERTURBED ',txt]);set(s,'interpreter','none')
-print_mul(sprintf('%s_semivar',txt))
-
 if nsaves<30,
     isimplot=1:1:nsaves;
 else
@@ -128,6 +134,19 @@ figure(5);set_paper;
 visim_plot_sim(V,isimplot,cax,FS);
 s=suptitle(['UNCONDITIONAL ',txt]);set(s,'interpreter','none')
 print_mul(sprintf('%s_reals_uncon',txt))
+
+return
+
+
+[g_uncon,hc_uncon]=visim_plot_semivar_real(V,[45 135],[10 10],[20 20],[1 1].*dx,0);
+[g_pert,hc_pert]=visim_plot_semivar_real(Vpert,[45 135],[10 10],[20 20],[1 1].*dx,0);
+figure(3);;set_paper;;clf;
+for i=1:2;
+subplot(1,2,i);
+plot(hc_pert{i},g_pert{i},'k-',hc_uncon{i},g_uncon{i},'r-');
+end
+s=suptitle(['PERTURBED ',txt]);set(s,'interpreter','none')
+print_mul(sprintf('%s_semivar',txt))
 
 save([txt,'.mat']);
 
