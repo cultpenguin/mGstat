db940dc7de6fc88b639c98318e773e3a266b015b
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2009-09-11 12:41:57 +0000

    Efficient FFT-MA-2D algorithm available for unconditional simulation, and perturbed gradual deformation.

--- fft_ma_2d.m
+++ fft_ma_2d.m
@@ -1,41 +1,100 @@
-function [out z pos]=FFT_MA_2D(ny,nx,Nly,Nlx,cell,h_min,h_max,gmean,gvar,func,lim,r,z)
+% fft_ma_2d : 
+% Call : 
+%    [out,z]=fft_ma_2d(x,y,Va,options)
+%
+%    x: array, ex : x=1:1:80:
+%    y: array, ex : y=1:1:50:
+%    Va: variogram def, ex : Va="1 Sph (10,.4,30)";
+% 
+%
+% "
+%   Ravalec, M.L. and Noetinger, B. and Hu, L.Y.},
+%   Mathematical Geology 32(6), 2000, pp 701-723
+%   The FFT moving average (FFT-MA) generator: An efficient numerical
+%   method for generating and conditioning Gaussian simulations
+% "  
+%
+% Example:
+%  x=[1:1:50];y=1:1:80;
+%  Va='1  Sph(10,.25,30)';
+%  [out,z]=fft_ma_2d(x,y,Va);
+%  imagesc(x,y,out);colorbar
+%
+%
+%  x=[1:1:50];y=1:1:80;
+%  Va='1  Sph(10,.25,30)';
+%  [out1,z_rand]=fft_ma_2d(x,y,Va);
+%  ii=300:350;
+%  z_rand(ii)=randn(size(z_rand(ii)));
+%  options.z_rand=z_rand;
+%  [out2,z_rand2]=fft_ma_2d(x,y,Va,options);
+%  subplot(1,3,1),imagesc(x,y,[out1]);colorbar;axis image;cax=caxis;
+%  subplot(1,3,2),imagesc(x,y,[out2]);caxis(cax);colorbar;axis image
+%  subplot(1,3,3),imagesc(x,y,[out2-out1]);colorbar;axis image
+%
 
-% The FFT-MA algorithm in 2D
-% Call: out=FFT_MA_2D(ny,nx,cell,h_min,h_max,gmean,gvar,func,lim,r,z);
-% mode: 1) Spherical, 2) Exponential, 3) Gaussian
-% r is a number beteen 0 and pi/2
-% lim is a 1x2 array
-% gvar
-% gmean
-% h_min
-% h_max
-% cell
-% Knud S. Cordua (June 2009)
 
-if nargin<11
-    lim=0;
-    r=0;
+
+%
+function [out,z_rand,options]=fft_ma_2d(x,y,Va,options)
+
+
+options.null='';
+nx=length(x);
+ny=length(y);
+dx=x(2)-x(1);
+dy=y(2)-y(1);
+cell=dx;
+
+if ~isstruct(Va);
+    Va=deformat_variogram(Va);
 end
-lim=lim/cell;
+
+if isfield(options,'gmean');
+    gmean=options.gmean;
+else
+    gmean=0;
+end
+if isfield(options,'gvar');
+    gvar=options.gvar;
+else
+    gvar=sum([Va.par1]);
+end
+
+if isfield(options,'Nly');
+    Nly=options.Nly;
+else
+    Nly=30;
+end
+
+if ~isfield(options,'Nlx');options.Nlx=30;end
+if ~isfield(options,'Nly');options.Nly=30;end
+
+
+% ONLY WORKS FOR ONE SEMIVRAIOHGRAM MODEL !!
+par2=Va(1).par2;
+h_max=par2(1);
+if length(par2)>1
+    h_min=h_max*par2(2);
+    ang=par2(3);
+else
+    h_min=h_max;
+    ang=0;
+end
+
 h_max=h_max/cell;
 h_min=h_min/cell;
 dx=cell;
 cell=1;
-Nx=nx;
-Ny=ny;
-nx=nx+Nlx*ceil(h_max/cell);
-ny=ny+Nly*ceil(h_min/cell);
+nx_org=nx;
+ny_org=ny;
+nx=nx+options.Nlx*ceil(h_max/cell);
+ny=ny+options.Nly*ceil(h_min/cell);
 
 if 2*h_min>cell*ny || 2*h_max>cell*nx
     error('Warning: A range is too long - extend the grid size')
 end
 
-ang=0;
-if h_min>h_max
-    disp('Warning:')
-    disp('Choose h_min<=h_max because h_max is the direction of maximum continuity.')
-    disp('Use the input "ang" to change the direction of maximum continutiy')
-end
 
 a_max=h_max;
 a_min=h_min;
@@ -56,63 +115,50 @@ h_max=h_x*sin(ang)+h_y*cos(ang);
 h_min_rs=h_min;
 h_max_rs=gamma*h_max;
 dist=sqrt(h_min_rs.^2+h_max_rs.^2);
-if func==1
-    dist(find(dist>a_min))=a_min;
+% calc semiavriogram
+Va2=Va;
+try
+    Va2.par2=Va.par2(1)*Va.par2(2);
+end
+C=gvar-semivar_synth(Va2,dist);
+
+if isfield(options,'z_rand')
+    z_rand=options.z_rand;
+else
+    z_rand=randn(ny,nx);
 end
 
-if func==1 % Spherical
-    C=1-(1.5*(dist./a_min)-0.5*(dist./a_min).^3);
-elseif func==2 % Exponential
-    C=exp(-3*dist./a_min);
-elseif func==3 % Gaussian
-    C=exp(-3*dist.^2./a_min.^2);
-else % Ricker
-    dx=1;
-    t0=0;
-    dt=1.061394451537358e-010;
-    time=dist*dt;
-    f0=100*10^6;
-    C=(1-2*pi^2*f0^2*(time-t0).^2).*exp(-pi^2*f0^2*(time-t0).^2);
-end
 
-figure(1)
-plot(C)
-
-%randn('seed',50)
-if nargin<13
-    z=sqrt(gvar)*randn(ny,nx);
-end
-
-%for i=1:100
-    if sum(lim)>0
-
-    pos(1)=round(1+rand(1)*(Nx-1));
-    pos(2)=round(1+rand(1)*(Ny-1));
-    [xx,yy]=meshgrid(1:nx,1:ny);
-    used=xx.*0;
-    used(abs(xx-pos(1))<lim(1) & abs(yy-pos(2))<lim(2))=1;
-    notused=xx.*0+1;
-    notused(abs(xx-pos(1))<lim(1) & abs(yy-pos(2))<lim(2))=0;
-    z=z.*notused+used.*z*cos(r)+used.*randn(size(used)).*used*sin(r)*sqrt(gvar);
-    pos=pos*dx;
+% Gradual Deformation ?
+if isfield(options,'lim');
+    x0=dx.*(nx-nx_org)/2;
+    y0=dy.*(ny-ny_org)/2;
+    x0=0;y0=0;
+    
+    if ~isfield(options,'gdm_step'); options.gdm_step=pi; end
+        
+    if isfield(options,'pos');
+        [options.used]=set_resim_data(x,y,z_rand,options.lim,options.pos+[x0 y0]);
+    else
+        x0=cell*ceil(rand(1)*nx_org);
+        y0=cell*ceil(rand(1)*ny_org);
+        options.pos=[x0 y0];
+        
+        [options.used]=set_resim_data(x,y,z_rand,options.lim,options.pos);
     end
+    ii=find(options.used==0);
+    z_rand_new=randn(size(z_rand(ii)));
+    z_rand(ii) = grad_deform(z_rand(ii),z_rand_new,options.gdm_step);     
+end
 
-%    S=fft2(C);
-%    Z=fft2(z);
-%    G=sqrt(cell*S);
-%    out1=G.*Z;
-%    out3=fftshift(real(ifft2(out1)),1);
-%    out3=real(ifft2(out1));
-%    out4=reshape(out3,ny,nx);
-     
-      out1=reshape(real(ifft2(sqrt(cell*fft2(C)).*fft2(z))),ny,nx);
-      out=out1(1:Ny,1:Nx)+gmean;
-      
-%      figure(20),subplot(1,2,1),imagesc(out),drawnow,title(sprintf('%i',i)),caxis([2.5 8.5])
-%      if i>1
-%          subplot(1,2,2),imagesc(out-out_old),axis image,caxis([-1 1])
-%      end
-%      out_old=out;
-%      pause
-% end
+z=gvar.*z_rand;
+
+options.out1=reshape(real(ifft2(sqrt(cell*fft2(C)).*fft2(z))),ny,nx);
+
+out=options.out1(1:ny_org,1:nx_org)+gmean;
+
+options.nx=nx;
+options.ny=ny;
+options.nx_org=nx_org;
+options.ny_org=ny_org;
 
