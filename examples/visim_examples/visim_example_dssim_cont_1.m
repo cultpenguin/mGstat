65a2a2102d8bed7604d74ddd22d51c90acfcfb5c
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2010-01-18 17:06:50 +0000

    DSSIM visim examples

---
+++ examples/visim_examples/visim_example_dssim_cont_1.m
@@ -0,0 +1,21 @@
+V=visim_init(1:1:61,1:1:61);
+V.parfile='visim_example_dssim_cont_1';
+d1=randn(1,2000)*sqrt(.2)+8;
+d2=randn(1,1200)*sqrt(.1)+10;
+d3=randn(1,2800)*sqrt(.1)+13;
+d_target=[d1,d2,d3]';
+V.refhist.fname='dssim_target.eas';
+write_eas(V.refhist.fname,d_target); % write target distribution
+V.ccdf=1;                 % use DSSIM
+V.refhist.do_discrete=0;  % Assume continious target histogram
+V.nsim=10;
+V=visim_init(V); 
+V.Va.it=3;     % Choose Gaussian semivariogram
+V.Va.a_hmax=15; % correlation length (direction of max continuity)
+V.Va.a_hmin=15; % correlation length (direction of min continuity)
+V=visim(V);
+figure(1);visim_plot_sim(V);
+print_mul('visim_example_dssim_cont_sim');
+figure(2);visim_plot_hist(V);
+print_mul('visim_example_dssim_cont_hist');
+
---
+++ examples/visim_examples/visim_example_dssim_cont_2.m
@@ -0,0 +1,19 @@
+V=visim_init(1:1:61,1:1:61);
+V.parfile='visim_example_dssim_cont_2';
+d_target=[1 10 10 40]';
+V.refhist.fname='dssim_target_discrete.eas';
+write_eas(V.refhist.fname,d_target); % write target distribution
+V.ccdf=0;                 % use DSSIM
+V.refhist.do_discrete=0;  % Assume continious target histogram
+V.nsim=10;
+V=visim_init(V); 
+V.Va.it=3;     % Choose Gaussian semivariogram
+V.Va.a_hmax=15; % correlation length (direction of max continuity)
+V.Va.a_hmin=15; % correlation length (direction of min continuity)
+V=visim(V);
+
+figure(1);visim_plot_sim(V);
+print_mul('visim_example_dssim_cont_1_sim');
+figure(2);visim_plot_hist(V);
+print_mul('visim_example_dssim_cont_1_hist');
+
