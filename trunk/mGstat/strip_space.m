291056256af5c49abc65fc92d694238e0191208f
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2010-01-22 08:26:47 +0000

    bug removing leading space

--- trunk/mGstat/strip_space.m
+++ trunk/mGstat/strip_space.m
@@ -12,39 +12,47 @@
 % EX :
 %  a='    Hei Ho Here We Go      ';
 % ['''',strip_space(a),'''']
-% ans = 
+% ans =
 % 'Hello World'
 %
 %% strip leading space
 % ['''',strip_space(a,1),'''']
 %% strip trailing space
 % ['''',strip_space(a,2),'''']
-% 
+%
 
 % TMH / 2004
 %
 function txt=strip_space(txt,type);
-  if nargin==1
-      type=0;
-  end
+if nargin==1
+    type=0;
+end
 
-  if type==0
-      txt=strtrim(txt); % OCTAVE COMPLIANT
-      return
-  end
-  
-  % leading space
-  if (type==1)
+if type==0
+    txt=strtrim(txt); % OCTAVE COMPLIANT
+    return
+end
+
+% leading space
+if (type==1)
     ispace=strfind(txt,' ');
-    if (ispace(1)==1);
-        % We have leading blanks
-        ilast=find(diff(ispace)>1);i_last=ilast(1)-1;
-        igood=setxor(1:ilast,1:1:length(txt));
-        txt=txt(igood);
+    if ~isempty(ispace)
+        
+        
+        if (ispace(1)==1);
+            % We have leading blanks
+            if length(ispace)==1
+                ilast=1;
+            else
+                ilast=find(diff(ispace)>1);i_last=ilast(1)-1;
+            end
+            igood=setxor(1:ilast,1:1:length(txt));
+            txt=txt(igood);
+        end
     end
     txt=regexprep(txt,'\< ',''); % NOT OCTAVE COMPLIANT
-  end
-  % trailing space
-  if (type==2)
-      txt=deblank(txt);  
-  end
+end
+% trailing space
+if (type==2)
+    txt=deblank(txt);
+end
