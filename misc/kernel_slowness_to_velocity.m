3fd5b8b441b72dfd26c403ec2e4d89ca08de65bb
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2010-03-30 19:54:21 +0000

    Proper SLOWNESS to VELOCITY parameterization change.

--- misc/kernel_slowness_to_velocity.m
+++ misc/kernel_slowness_to_velocity.m
@@ -1,9 +1,14 @@
-% kernel_slowness_to_velocity : converts 
+% kernel_slowness_to_velocity : converts from slowness to velocity parameterizations
 %
 % CALL:
-%  G_vel=kernel_slowness_to_velocity(G,V);
+%   G_vel=kernel_slowness_to_velocity(G,V);
+% or 
+%   [G_vel,v_obs]=kernel_slowness_to_velocity(G,V,t);
+% or
+%   [G_vel,v_obs,Cd_v]=kernel_slowness_to_velocity(G,V,t,Cd);
+%
 % 
-function G_vel=kernel_slowness_to_velocity(G,V);
+function [G_vel,v_obs,Cd_v]=kernel_slowness_to_velocity(G,V,t,Cd_org);
 
 nxy=prod(size(V));
 varr=V(:);
@@ -19,9 +24,24 @@ if (sum(size(G)==size(V))==2)
 end
 
 if size(G,2)==nxy    
-    t=G*(1./varr);
+    
+    l=sum(G');
+    
+    % NORMALIZE KERNEL FOR VELOCITY
+    t_app=G*(1./varr);
     for i=1:nxy
-        G_vel(:,i)=(G(:,i)/varr(i))./t;
+        G_vel(:,i)=(G(:,i)/varr(i))./t_app;
     end
-    return
+    
+    if nargin>2
+        % COMPUTE APPARENT VELOCITIES FROM TRAVEL TIME
+        v_obs=l./t;
+    end
+    
+    if nargin>3
+        % COMPUTE Cd_velocity from Cd_traveltime
+        v_obs_noise=l./(t+sqrt(diag(Cd_org)'));
+        Cd_v=diag(v_obs-v_obs_noise).^2;
+    end
+    
 end
