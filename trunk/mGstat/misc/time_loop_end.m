7668371429ce16b34fc461912810e4236ea440b5
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2012-02-21 09:10:23 +0000

    a few updates ...

--- trunk/mGstat/misc/time_loop_end.m
+++ trunk/mGstat/misc/time_loop_end.m
@@ -40,7 +40,8 @@ t_end= tnow+n_ite_left*t_per_iteration;
 t_per_iteration_seconds=t_per_iteration*24*3600;
 t_left_seconds=t_left*24*3600;
 
-t_end_txt=sprintf('end:%s sec-per-ite = %4.1f s',datestr(t_end,datestr_format),t_per_iteration_seconds);
+t_end_txt=sprintf('%s',datestr(t_end,datestr_format));
+%t_end_txt=sprintf('end:%s sec-per-ite = %4.1f s',datestr(t_end,datestr_format),t_per_iteration_seconds);
 if nargout==0
     disp(t_end_txt)
 end
