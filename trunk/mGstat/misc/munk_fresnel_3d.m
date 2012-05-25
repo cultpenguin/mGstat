ea8949f866e8dfce6a43eea61aaff699f54352bf
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2012-05-25 09:22:09 +0000

    misc updates

--- trunk/mGstat/misc/munk_fresnel_3d.m
+++ trunk/mGstat/misc/munk_fresnel_3d.m
@@ -3,7 +3,14 @@
 % See Jensen, Jacobsen, Christensen-Dalsgaard (2000) Solar Physics 192.
 %
 % Call :
+
 % S=munk_fresnel_3d(T,dt,alpha,As,Ar,K);
+% T : dominant period
+% dt : 
+% alpha : degree of cancellation 
+% As : Amplitude fo the wavefield propagating from the source
+% Ar : Amplitude fo the wavefield propagating from the receiver
+% K : normalization factor
 function S=munk_fresnel_3d(T,dt,alpha,As,Ar,K);
   
   if nargin<2,    
