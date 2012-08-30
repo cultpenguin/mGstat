8c572b5e5f7b6546f72a3278fc1867907707331b
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2012-08-30 09:17:38 +0000

    Misc updates....

--- print_mul.m
+++ print_mul.m
@@ -11,7 +11,7 @@
 %            [0]: no transparency (def)
 %            [1]: white as transparent
 %            ['red']: red as transparent
-%    res : resolution 
+%    res : resolution
 %            [300] (def)
 %    do_watermark : add fname as watermark to figure in lower right corner
 %            [0] no watermark(def)
@@ -20,11 +20,11 @@
 % Ex :
 %   print_mul('test') % prints test.eps, test.png, and test.pdf
 %   print_mul('test',1,1) % prints test.eps, test.png, and test.pdf
-%                         % test.png will be trimmed and white set as 
+%                         % test.png will be trimmed and white set as
 %                         % transparent color
 %
 %   print_mul('test',0,'black',600,1) % prints test.eps, test.png, and test.pdf
-%                         % test.png will NOT be trimmed and BLACK set as 
+%                         % test.png will NOT be trimmed and BLACK set as
 %                         % transparent color. resolution is set to 600dpi
 %                         % and a watermark is added
 %
@@ -46,27 +46,32 @@ fname=space2char(fname);
 if do_watermark==1
     watermark(fname);
 end
-    
-P{1}.type='-dpng';P{1}.ext='.png';
-P{2}.type='-dpdf';P{2}.ext='.pdf';
-%P{3}.type='-depsc';P{3}.ext='.eps';
+
+fname=space2char(fname);
+
+i=0;
+i=i+1;P{i}.type='-dpng';P{i}.ext='.png';
+%i=i+1;P{i}.type='-depsc';P{i}.ext='.eps';
+i=i+1;P{i}.type='-dpdf';P{i}.ext='.pdf';
 
 for i=1:length(P)
     res_string=sprintf('-r%d',res);
     file_out=[fname,P{i}.ext];
     %file_out=[fname,'];
     try
-        print(gcf, P{i}.type,res_string,file_out  )
+        print(gcf, P{i}.type,res_string,[file_out])
     catch
         disp(sprintf('%s : failed to print %s as %s',mfilename,fname,P{i}.ext))
     end
 end
 
+%% SAVE FIG FILE
 if (save_fig==1)
     saveas(gcf,[fname,'.fig'],'fig');
 end
 
-% TRIM IMAGE USING MOGRIFY
+
+%% TRIM IMAGE USING MOGRIFY
 
 if (trim==1)|(transp~=0)
     if isunix
@@ -77,29 +82,25 @@ if (trim==1)|(transp~=0)
         %disp(sprintf('%s : trimming only supported on Unix',mfilename))
     end
     
+    % TRANSPAREMCY
+    if isstr(transp)
+        transp_cmd=['-transparent ',transp];
+    else
+        if transp==1
+            transp_cmd=['-transparent white'];
+        else
+            transp_cmd='';
+        end
+    end
     
+    if trim==1;
+        trim_cmd='-trim';
+    else
+        trim_cmd='';
+    end
     
-  
-  % TRANSPAREMCY
-  if isstr(transp)
-      transp_cmd=['-transparent ',transp];
-  else
-      if transp==1
-          transp_cmd=['-transparent white'];
-      else
-          transp_cmd='';
-      end
-  end
-  
-  if trim==1;
-      trim_cmd='-trim';
-  else
-      trim_cmd='';
-  end
-  
-  
-  % MOGRIFY PNG FILE
-  if exist(mogrifybin,'file')
+    % MOGRIFY PNG FILE
+    if exist(mogrifybin,'file')
         cmd=sprintf('%s %s %s %s.png',mogrifybin,trim_cmd,transp_cmd,fname);
         [status,result]=system(cmd);
         if status~=0; disp(sprintf('%s : %s',mfilename,result));end
