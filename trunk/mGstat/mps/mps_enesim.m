59673cbd54890083d78fe06c9ebc367c088c6f15
cultpenguin <cultpenguin@29461c38-f360-4d2e-b366-262630b97e27>
2015-02-03 13:04:22 +0000

    2D ENESIM and DSIM

--- trunk/mGstat/mps/mps_enesim.m
+++ trunk/mGstat/mps/mps_enesim.m
@@ -1,12 +1,10 @@
-% mps_dsim: Direct simulation
+% mps_enesim: sequential simulation of mulitple points statistical model
 %
-% VERY simple implementation of Direct Simulation
-% Currently only works for relatively small simulations grids, and
-% categorical TI
-% See Mariethoz et al. (2010) for details
+% Very simple 2D implementation of the MPS approach 
+% described by Guardiano and Srivastava (1993)
 %
 % Call
-%    mps_dsim(TI,SIM,options)
+%     [out,options]=mps_enesim(TI,SIM,options)
 %
 %  TI: [ny,nx] 2D training image (categorical variables
 %  SIM: [ny2,nx2] 2D simulation grid. 'NaN' indicates an unkown value
@@ -18,9 +16,9 @@
 %  options.plot    [int]: [0]:none, [1]:plot cond, [2]:storing movie (def=0)
 %  options.verbose [int]: [0] no info to screen, [1]:some info (def=1)
 %
-% See also: mps_enesim
+% See also: mps_dsim
 %
-function [out,options]=mps_dsim(TI_data,SIM_data,options)
+function [out,options]=mps_enesim(TI_data,SIM_data,options)
 if nargin<3
     options.null='';
 end
@@ -55,9 +53,13 @@ SIM.y=1:1:SIM.ny;
 N_SIM=numel(SIM.D);
 
 
+options.E=SIM.D.*0.*NaN;
+options.N_DROPPED=SIM.D.*0.*NaN;
+    
+
+
 if options.plot>2
-    writerObj = VideoWriter('dsim');
-    %writerObj = VideoWriter(vname,'MPEG-4'); % Awful quality ?
+    writerObj = VideoWriter('enesim');
     writerObj.FrameRate=30;
     writerObj.Quality=90;
     open(writerObj);
@@ -80,11 +82,12 @@ N_PATH=length(i_path);
 %% BIG LOOP OVER RANDOM PATH
 
 for i=1:N_PATH; %  % START LOOOP OVER PATH
-    
+
     if options.verbose>0
         if ((i/100)==round(i/100))&(options.plot>-1)
-            progress_txt(i,N_SIM,mfilename);
-            progress_txt(i,N_PATH,mfilename);
+        %    progress_txt(i,N_SIM,mfilename);
+        %    progress_txt(i,N_PATH,mfilename);
+        disp(sprintf('%s: %03d/%02d',mfilename,i,N_PATH))
         end
     end
     
@@ -96,7 +99,7 @@ for i=1:N_PATH; %  % START LOOOP OVER PATH
     end
     
     
-    
+     
     %% FIND n_cond CONDITIONAL POINT, find L
     % V value of conditional point
     % L relative location of conditional point
@@ -105,6 +108,8 @@ for i=1:N_PATH; %  % START LOOOP OVER PATH
         i_good=find(~isnan(SIM.D));
         
         [d,options]=mps_get_distance(SIM.ny,SIM.nx,i_node,i_good,options);
+        %d=options.DIST(i_node,i_good);
+        %keyboard
         ii=1:length(d);
         s=sortrows([ii(:) d(:)],2);
         use_cond=min([options.n_cond length(d)]);
@@ -118,83 +123,33 @@ for i=1:N_PATH; %  % START LOOOP OVER PATH
         end
         
     end
+    N_COND=length(V);
     
     
-    
-    % LOOP OVER TI,
-    % choose random starting point
-    j_start=ceil(rand(1)*N_TI);
-    j_arr(1:(N_TI-j_start+1))=j_start:1:N_TI;
-    j_arr((N_TI-j_start+2):N_TI)=1:(j_start-1);
-    
-    ij=0;
-    DIS_MIN=1e+5;
-    for j=j_arr %
-        ij=ij+1;
-        [iy_ti,ix_ti]=ind2sub_2d([TI.ny,TI.nx],j);
-        if i==1;
-            iy_ti_min=iy_ti;
-            ix_ti_min=ix_ti;
-            %SIM.D(iy,ix)=TI.D(iy_ti,ix_ti);
-            break
-        else
-            % GET INDEX CENTER INDEX IN TI
-            %P=repmat([iy_ti ix_ti],size(L,1),1);
-            
-            % COMPUTE DISTANCE
-            DIS=0;
-            for k=1:size(L,1);
-                iy_test=L(k,1)+iy_ti;
-                ix_test=L(k,2)+ix_ti;
-                
-                if ((iy_test>0)&&(iy_test<=TI.ny)&&(ix_test>0)&(ix_test<=TI.nx))
-                    if TI.D(iy_test,ix_test)==V(k);
-                        DIS=DIS+0;
-                    else
-                        DIS=DIS+1;
-                    end
-                else
-                    DIS=DIS+1;
-                end
-            end
-            
-            %% keep track of the pattern with the smallesty dist so far
-            if DIS<DIS_MIN
-                DIS_MIN=DIS;
-                iy_ti_min=iy_ti;
-                ix_ti_min=ix_ti;
-            end
-            
-            % store how many iteration in the TI is performed
-            options.C(iy,ix)=ij;
-            
-            %% STOP, if perfect match has been reached
-            if DIS==0
-                iy_ti_min=iy_ti;
-                ix_ti_min=ix_ti;
-                %SIM.D(iy,ix)=TI.D(iy_ti,ix_ti);
-                
-                break
-            end
-            
-            %% STOP, if maximum number of allowed iterations have been reached
-            if ij>=options.n_max_ite;
-                %SIM.D(iy,ix)=TI.D(iy_ti_min,ix_ti_min);
-                break;
-            end
-            
-            
+    %% OLDSTYLY GUARIDANO AND SRIVASTBA
+    % GET FULL CONDITIONAL
+    N_PDF=0;
+    if N_COND==0
+        [C_PDF,N_PDF,TI]=mps_get_conditional_from_template(TI,[],[]);
+    else
+        for ic=1:N_COND
+            c_arr=(1:(N_COND-ic+1));
+            [C_PDF,N_PDF,TI]=mps_get_conditional_from_template(TI,V(c_arr),L(c_arr,:));
+            if N_PDF>0, break; end
+            disp(sprintf('%s : PRUNING: dropping a node %02d/%02d at[ix,iy]=[%d,%d]',mfilename,N_COND-ic,N_COND,ix,iy))
+            options.N_DROPPED(iy,ix)=ic;
         end
     end
-    % UPDATE SIM GRID WITH CONDITIONAL VALAUE
-    SIM.D(iy,ix)=TI.D(iy_ti_min,ix_ti_min);
+    options.E(iy,ix)=entropy(C_PDF);
     
-    % GET FULL CONDITIONAL TO COMPUTE ENTROPY
-    options.compute_entropy=0;
-    if options.compute_entropy==1;
-        [C_PDF,TI]=mp_get_conditional_from_template(TI,V,L);
-        options.E(iy,ix)=entropy(C_PDF);
+    %% DRAW REALIZARTION FROM C_PDF
+    sim_val=min(find(cumsum(C_PDF)>rand(1)))-1;
+    try
+    SIM.D(iy,ix)=sim_val;
+    catch
+        keyboard
     end
+    
     
     %% PLOT START
     if options.plot>0
@@ -217,34 +172,38 @@ for i=1:N_PATH; %  % START LOOOP OVER PATH
             colormap(cmap_linear([1 1 1 ; 0 0 0; 1 0 0]))
             axis image
         end
-        if options.plot>1
-            subplot(1,2,2);
-            hold on
-            im_sim=imagesc(SIM.D);
-            caxis([-1 1]);
-            colormap(cmap_linear([1 1 1 ; 0 0 0; 1 0 0]))
-            axis image
-            plot(ix,iy,'go','MarkerSize',12)
-            for l=1:size(L,1)
-                plot([ix ix+L(l,2)],[iy iy+L(l,1)],'g-')
-            end
-            hold off
-            
-        end
+         if options.plot>1
+              subplot(1,2,2);
+              
+              figure_focus(2);
+              subplot(1,2,2);
+              im_sim=imagesc(SIM.D);
+              caxis([-1 1]);
+              colormap(cmap_linear([1 1 1 ; 0 0 0; 1 0 0]))
+              axis image
+              
+              hold on
+              plot(ix,iy,'go','MarkerSize',12)
+              for l=1:size(L,1)
+                  plot([ix ix+L(l,2)],[iy iy+L(l,1)],'g-')
+              end
+              hold off
+             
+         end
         
-        %         if options.plot>1
-        %             subplot(1,2,1);
-        %             im=imagesc(TI.D);axis image;
-        %             caxis([-1 1]);
-        %             hold on
-        %             plot(ix_ti_min,iy_ti_min,'go','MarkerSize',12)
-        %             for l=1:size(L,1)
-        %                 plot([ix_ti_min ix_ti_min+L(l,2)],[iy_ti_min iy_ti_min+L(l,1)],'g-')
-        %             end
-        %             hold off
-        %         end
-        
-        
+%         if options.plot>1
+%             subplot(1,2,1);
+%             im=imagesc(TI.D);axis image;
+%             caxis([-1 1]);
+%             hold on
+%             plot(ix_ti_min,iy_ti_min,'go','MarkerSize',12)
+%             for l=1:size(L,1)
+%                 plot([ix_ti_min ix_ti_min+L(l,2)],[iy_ti_min iy_ti_min+L(l,1)],'g-')
+%             end
+%             hold off
+%         end
+         
+         
         if options.plot>2
             frame = getframe(gcf);
             writeVideo(writerObj,frame);
