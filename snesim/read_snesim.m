f3ec22588cb3ab6940cac54c2708f0873572e80e
Thomas Mejer Hansen <cultpenguin@gmail.com>
2015-08-04 22:38:01 +0200

    starting up for SNESIM 10

--- snesim/read_snesim.m
+++ snesim/read_snesim.m
@@ -1,19 +1,27 @@
-% read_snesim : Read SNESIM parameter file
-% 
-%  S = read_snesim(filename);
-%  
+% read_snesim : Read SNESIM (v10) parameter file
+%
+%  S = read_snesim_10(filename,read_data);
+%     filename [string]: snesim parameter file (def:'snesim.par')
+%     read_data [integer]: optionally read input/output data files
+%          read_data=0; % read no data
+%          read_data=1; % read output from visim
+%          read_data=2; % read all input and output from visim
+%
 %  S is a Matlab structure will all options for running SNESIM
 %
-% See also: write_snesim, snesim
+% See also: write_snesim_10, snesim
 %
-function obj=read_snesim(filename)
+function obj=read_snesim_10(filename,read_data)
 
 if nargin==0;
     filename='snesim.par';
 end
+if nargin<2
+    read_data=0;
+end
 
 if exist(filename,'file')~=2,
-    help read_snesim
+    help(mfilename)
     obj=[];
     return
 end
@@ -42,13 +50,14 @@ while (head==1)
     end
 end
 
-
 % File with cond data
 line=fgetl(fid);
 [fname]=get_string(line);
 obj.fconddata.fname=fname;
 try
-    obj.fconddata.data=read_eas(fname);
+    if (read_data>1);
+        obj.fconddata.data=read_eas(fname);
+    end
 catch
     disp(sprintf('%s : Could not read conditional data : %s',mfilename,fname));
 end
@@ -71,7 +80,6 @@ obj.cat_code=sscanf(line,'%f');
 line=fgetl(fid);
 obj.pdf_target=sscanf(line,'%f');
 
-
 % Use vertical prop
 line=fgetl(fid);
 obj.use_vert_prop=sscanf(line,'%d');
@@ -79,14 +87,15 @@ obj.use_vert_prop=sscanf(line,'%d');
 line=fgetl(fid);
 obj.fvertprob.fname=get_string(line);
 try
-    obj.fvertprob.data=read_eas(obj.fvertprob.fname);
+    if read_data>1
+        obj.fvertprob.data=read_eas(obj.fvertprob.fname);
+    end
 end
 
-% Target pdf
+% servo system
 line=fgetl(fid);
 data=sscanf(line,'%f');
-obj.pdf_target_repro=data(1);
-obj.pdf_target_par=data(2);
+obj.servosystem=data(1);
 
 % Debug level
 line=fgetl(fid);
@@ -100,7 +109,9 @@ obj.fdebug.fname=get_string(line);
 line=fgetl(fid);
 obj.out.fname=get_string(line);
 try
-    obj.out.data=read_eas(obj.out.fname);
+    if (read_data>0)
+        obj.out.data=read_eas(obj.out.fname);
+    end
 catch
     obj.out.data=[];
 end
@@ -112,88 +123,123 @@ obj.nsim=sscanf(line,'%f');
 % DIM INFO
 line=fgetl(fid);
 tmp=sscanf(line,'%d %f %f');
-obj.nx=tmp(1);obj.xmn=tmp(2);obj.xsiz=tmp(3);
+obj.dim.nx=tmp(1);obj.dim.xmn=tmp(2);obj.dim.xsiz=tmp(3);
 line=fgetl(fid);
 tmp=sscanf(line,'%d %f %f');
-obj.ny=tmp(1);obj.ymn=tmp(2);obj.ysiz=tmp(3);
+obj.dim.ny=tmp(1);obj.dim.ymn=tmp(2);obj.dim.ysiz=tmp(3);
 line=fgetl(fid);
 tmp=sscanf(line,'%d %f %f');
-obj.nz=tmp(1);obj.zmn=tmp(2);obj.zsiz=tmp(3);
-obj.x=[0:1:obj.nx-1]*obj.xsiz+obj.xmn;
-obj.y=[0:1:obj.ny-1]*obj.ysiz+obj.ymn;
-obj.z=[0:1:obj.nz-1]*obj.zsiz+obj.zmn;
+obj.dim.nz=tmp(1);obj.dim.zmn=tmp(2);obj.dim.zsiz=tmp(3);
+obj.dim.x=[0:1:obj.dim.nx-1]*obj.dim.xsiz+obj.dim.xmn;
+obj.dim.y=[0:1:obj.dim.ny-1]*obj.dim.ysiz+obj.dim.ymn;
+obj.dim.z=[0:1:obj.dim.nz-1]*obj.dim.zsiz+obj.dim.zmn;
 
 
 % RSEED
 line=fgetl(fid);
 obj.rseed=sscanf(line,'%f');
 
-% template
-line=fgetl(fid);
-obj.ftemplate.fname=get_string(line);
-try
-    obj.ftemplate.data=read_eas(obj.ftemplate.fname);
-end
+%% template
+%line=fgetl(fid);
+%obj.ftemplate.fname=get_string(line);
+%try
+%    obj.ftemplate.data=read_eas(obj.ftemplate.fname);
+%end
 
 % max cond data
 line=fgetl(fid);
 obj.max_cond=sscanf(line,'%f');
-% max data per octant
-line=fgetl(fid);
-obj.max_data_per_oct=sscanf(line,'%f');
-%  max data events
-line=fgetl(fid);
-obj.max_data_events=sscanf(line,'%f');
 
-%
+% max number of replicates
+line=fgetl(fid);
+obj.min_replicates=sscanf(line,'%f');
+
+% condition to LP
+line=fgetl(fid);
+tmp=sscanf(line,'%d %d');
+obj.condition_to_lp=tmp(1);
+obj.iauto=tmp(2);
+
+
+% tau1, tau2
+line=fgetl(fid);
+tmp=sscanf(line,'%f %f');
+obj.tau1=tmp(1);
+obj.tau2=tmp(2);
+
+% localprob
+line=fgetl(fid);
+obj.flocalprob.fname=get_string(line);
+
+% rotation and affinity
+line=fgetl(fid);
+tmp=sscanf(line,'%d');
+obj.frotaff.use=tmp(1);
+
+line=fgetl(fid);
+obj.frotaff.fname=get_string(line);
+
+line=fgetl(fid);
+tmp=sscanf(line,'%d');
+obj.frotaff.n_cat=tmp(1);
+for i=1:obj.frotaff.n_cat
+    line=fgetl(fid);
+    tmp=sscanf(line,'%f %f %f');
+    obj.frotaff.aff_xyz(i,:)=tmp;
+end
+
+
+% n multiple grids
 line=fgetl(fid);
 data=sscanf(line,'%f');
 obj.n_mulgrids=data(1);
-obj.n_mulgrids_w_stree=data(2);
 
 % Training image
 line=fgetl(fid);
-obj.fti.fname=get_string(line);
-try
-    obj.fti.data=read_eas(obj.fti.fname);
+obj.ti.fname=get_string(line);
+if (read_data>1)
+    obj.ti.data=read_eas(obj.ti.fname);
 end
+
 % ti dim
 line=fgetl(fid);
 data=sscanf(line,'%f');
-obj.nxtr=data(1);
-obj.nytr=data(2);
-obj.nztr=data(3);
-if obj.nztr==1
-    obj.ti=reshape(obj.fti.data,obj.nxtr,obj.nytr);
-else
-    obj.ti=reshape(obj.fti.data,obj.nxtr,obj.nytr,obj.nztr);
+obj.ti.nx=data(1);
+obj.ti.ny=data(2);
+obj.ti.nz=data(3);
+
+if (read_data>1)
+    if obj.ti.nz==1
+        obj.ti.data=reshape(obj.ti.data,obj.ti.nx,obj.ti.ny)';
+    else
+        obj.ti.data=reshape(obj.ti.data,obj.ti.nx,obj.ti.ny,obj.ti.nz);
+    end
 end
-%  max data events
 line=fgetl(fid);
-obj.fti.col_var=sscanf(line,'%f');
+obj.ti.col_var=sscanf(line,'%f');
 
 % Search radius
 line=fgetl(fid);
 data=sscanf(line,'%f');
-obj.hmax=data(1);
-obj.hmin=data(2);
-obj.hvert=data(3);
+obj.search_radius.hmax=data(1);
+obj.search_radius.hmin=data(2);
+obj.search_radius.hvert=data(3);
 % Search angles
 line=fgetl(fid);
 data=sscanf(line,'%f');
-obj.amax=data(1);
-obj.amin=data(2);
-obj.avert=data(3);
+obj.search_radius.amax=data(1);
+obj.search_radius.amin=data(2);
+obj.search_radius.avert=data(3);
 
-
-nsim=obj.nsim;
-nxyz=obj.nx*obj.ny*obj.nz;
-if (nsim>0)&(~isempty(obj.out.data))
-    try
-        if obj.nz==1,
-            obj.D=reshape(obj.out.data(1:(nsim*nxyz)),obj.nx,obj.ny,nsim);
+if (read_data>0)
+    nsim=obj.nsim;
+    nxyz=obj.dim.nx*obj.dim.ny*obj.dim.nz;
+    if (nsim>0)&(~isempty(obj.out.data))
+        %try
+        if obj.dim.nz==1,
+            obj.D=reshape(obj.out.data(1:(nsim*nxyz)),obj.dim.nx,obj.dim.ny,nsim)';
         else
-            obj.D=reshape(obj.out.data(1:(nsim*nxyz)),obj.nx,obj.ny,obj.nz,nsim);
+            obj.D=reshape(obj.out.data(1:(nsim*nxyz)),obj.dim.nx,obj.dim.ny,obj.dim.nz,nsim);
         end
         if nsim==1
             E=obj.D;
@@ -207,11 +253,11 @@ if (nsim>0)&(~isempty(obj.out.data))
         if (nsim~=obj.nsim),
             disp(sprintf('SETTING NSIM=%d TO MATCH SIM DATA',nsim));
         end
-    catch
-        disp(sprintf('FAILED TO LOAD DATA',nsim));
+        %catch
+        %    disp(sprintf('FAILED TO LOAD DATA',nsim));
+        %end
     end
 end
-
 
 
 fclose(fid);
--- snesim/write_snesim.m
+++ snesim/write_snesim.m
@@ -1,72 +1,277 @@
-% write_snesim : write SNESIM parameter file
+% read_snesim : Read SNESIM (v10) parameter file
 %
-% Ex. read/write snesim parameter file 
-%   V=read_snesim('snesim.par');
-%   V.nsim=10;
-%   write('snesim.par',V)
+%  S = read_snesim_10(filename,read_data);
+%     filename [string]: snesim parameter file (def:'snesim.par')
+%     read_data [integer]: optionally read input/output data files
+%          read_data=0; % read no data
+%          read_data=1; % read output from visim
+%          read_data=2; % read all input and output from visim
 %
-% See also: read_snesim, snesim
-% 
-function obj=write_snesim(obj,parfile)
-  
-  if nargin==0
-    help write_snesim
-    return;
-  end
+%  S is a Matlab structure will all options for running SNESIM
+%
+% See also: write_snesim_10, snesim
+%
+function obj=read_snesim_10(filename,read_data)
 
-  
-  if isstruct(obj)~=1
-    disp(sprintf('%s : 1st input parameter MUST be a snesim struvture',mfilename))
-    return;
-  end
+if nargin==0;
+    filename='snesim.par';
+end
+if nargin<2
+    read_data=0;
+end
 
-  if nargin==2
-    if isstr(parfile);
-      obj.parfile=parfile;
+if exist(filename,'file')~=2,
+    help(mfilename)
+    obj=[];
+    return
+end
+
+
+obj.parfile=filename;
+
+fid = fopen(filename,'r');
+
+
+% READ HEADER
+head=1;
+
+
+i=0;
+
+while (head==1)
+    i=i+1;
+    line=fgetl(fid);
+    obj.header{i}=line;
+    %disp(line)
+    if length(line)>=19
+        if (strcmp(line(1:19),'START OF PARAMETERS'))
+            head=0;
+        end
     end
-  end
+end
 
-  
-  parfile=obj.parfile;
-  
-  fid = fopen(parfile,'w');
+% File with cond data
+line=fgetl(fid);
+[fname]=get_string(line);
+obj.fconddata.fname=fname;
+try
+    if (read_data>1);
+        obj.fconddata.data=read_eas(fname);
+    end
+catch
+    disp(sprintf('%s : Could not read conditional data : %s',mfilename,fname));
+end
+% Cols
+line=fgetl(fid);
+obj.fconddata.cols=sscanf(line,'%d');
+obj.fconddata.xcol=  obj.fconddata.cols(1);
+obj.fconddata.ycol=  obj.fconddata.cols(2);
+obj.fconddata.zcol=  obj.fconddata.cols(3);
+obj.fconddata.vcol=  obj.fconddata.cols(4);
 
-  % MAKE OUT FILE NAME THE SAME AS THE PARAMETER FILE + .OUT
-  % [p,f]=fileparts(obj.parfile);
-  % obj.out.fname=sprintf('%s.out',f);
-  
-  fprintf(fid,'                  Parameters for snesim\n');
-  fprintf(fid,'                  ********************\n');
-  fprintf(fid,'                                      \n');
-  fprintf(fid,'START OF PARAMETERS                   \n');
+% Number for categories
+line=fgetl(fid);
+obj.ncat=sscanf(line,'%f');
+% category codes
+line=fgetl(fid);
+obj.cat_code=sscanf(line,'%f');
 
-  txt='';
-  fprintf(fid,'%s                    - %s\n',obj.fconddata.fname,txt);
-  fprintf(fid,'%d %d %d %d           - %s\n',obj.fconddata.xcol,obj.fconddata.ycol,obj.fconddata.zcol,obj.fconddata.vcol,txt);
-  fprintf(fid,'%d                    - %s\n',obj.ncat,txt);
-  fprintf(fid,'%d %d                    - %s\n',obj.cat_code(1),obj.cat_code(2),txt);
-  fprintf(fid,'%g %g                    - %s\n',obj.pdf_target(1),obj.pdf_target(2),txt);
-  fprintf(fid,'%d                    - %s\n',obj.use_vert_prop,txt);
-  fprintf(fid,'%s                    - %s\n',obj.fvertprob.fname,txt);
-  fprintf(fid,'%d %g                    - %s\n',obj.pdf_target_repro,obj.pdf_target_par,txt);
-  fprintf(fid,'%d                    - %s\n',obj.debug_level,txt);
-  fprintf(fid,'%s                    - %s\n',obj.fdebug.fname,txt);
-  fprintf(fid,'%s                    - %s\n',obj.out.fname,txt);
-  fprintf(fid,'%d                    - %s\n',obj.nsim,txt);
-  fprintf(fid,'%d %g %g                   - %s\n',obj.nx,obj.xmn,obj.xsiz,txt);
-  fprintf(fid,'%d %g %g                   - %s\n',obj.ny,obj.ymn,obj.ysiz,txt);
-  fprintf(fid,'%d %g %g                   - %s\n',obj.nz,obj.zmn,obj.zsiz,txt);
-  fprintf(fid,'%d                   - %s\n',obj.rseed,txt);
-  fprintf(fid,'%s                    - %s\n',obj.ftemplate.fname,txt);
-  fprintf(fid,'%d                    - %s\n',obj.max_cond,txt);
-  fprintf(fid,'%d                    - %s\n',obj.max_data_per_oct,txt);
-  fprintf(fid,'%d                    - %s\n',obj.max_data_events,txt);
-  txt='n_multiple_grids, n_multiple_grids_w_stree';
-  fprintf(fid,'%d %d                    - %s\n',obj.n_mulgrids,obj.n_mulgrids_w_stree,txt);
-  txt='';
-  fprintf(fid,'%s                    - %s\n',obj.fti.fname,txt);
-  fprintf(fid,'%d %d %d                   - %s\n',obj.nxtr,obj.nytr,obj.nztr,txt);
-  fprintf(fid,'%d                    - %s\n',obj.fti.col_var,txt);
-  fprintf(fid,'%g %g %g                    - %s\n',obj.hmax,obj.hmin,obj.hvert,txt);
-  fprintf(fid,'%g %g %g                    - %s\n',obj.amax,obj.amin,obj.avert,txt);
-  
+% Target pdf
+line=fgetl(fid);
+obj.pdf_target=sscanf(line,'%f');
+
+% Use vertical prop
+line=fgetl(fid);
+obj.use_vert_prop=sscanf(line,'%d');
+% vertical prop file
+line=fgetl(fid);
+obj.fvertprob.fname=get_string(line);
+try
+    if read_data>1
+        obj.fvertprob.data=read_eas(obj.fvertprob.fname);
+    end
+end
+
+% servo system
+line=fgetl(fid);
+data=sscanf(line,'%f');
+obj.servosystem=data(1);
+
+% Debug level
+line=fgetl(fid);
+obj.debug_level=sscanf(line,'%f');
+
+% DEBUG
+line=fgetl(fid);
+obj.fdebug.fname=get_string(line);
+
+% OUTPUT
+line=fgetl(fid);
+obj.out.fname=get_string(line);
+try
+    if (read_data>0)
+        obj.out.data=read_eas(obj.out.fname);
+    end
+catch
+    obj.out.data=[];
+end
+
+% NSIM
+line=fgetl(fid);
+obj.nsim=sscanf(line,'%f');
+
+% DIM INFO
+line=fgetl(fid);
+tmp=sscanf(line,'%d %f %f');
+obj.dim.nx=tmp(1);obj.dim.xmn=tmp(2);obj.dim.xsiz=tmp(3);
+line=fgetl(fid);
+tmp=sscanf(line,'%d %f %f');
+obj.dim.ny=tmp(1);obj.dim.ymn=tmp(2);obj.dim.ysiz=tmp(3);
+line=fgetl(fid);
+tmp=sscanf(line,'%d %f %f');
+obj.dim.nz=tmp(1);obj.dim.zmn=tmp(2);obj.dim.zsiz=tmp(3);
+obj.dim.x=[0:1:obj.dim.nx-1]*obj.dim.xsiz+obj.dim.xmn;
+obj.dim.y=[0:1:obj.dim.ny-1]*obj.dim.ysiz+obj.dim.ymn;
+obj.dim.z=[0:1:obj.dim.nz-1]*obj.dim.zsiz+obj.dim.zmn;
+
+
+% RSEED
+line=fgetl(fid);
+obj.rseed=sscanf(line,'%f');
+
+%% template
+%line=fgetl(fid);
+%obj.ftemplate.fname=get_string(line);
+%try
+%    obj.ftemplate.data=read_eas(obj.ftemplate.fname);
+%end
+
+% max cond data
+line=fgetl(fid);
+obj.max_cond=sscanf(line,'%f');
+
+% max number of replicates
+line=fgetl(fid);
+obj.min_replicates=sscanf(line,'%f');
+
+% condition to LP
+line=fgetl(fid);
+tmp=sscanf(line,'%d %d');
+obj.condition_to_lp=tmp(1);
+obj.iauto=tmp(2);
+
+
+% tau1, tau2
+line=fgetl(fid);
+tmp=sscanf(line,'%f %f');
+obj.tau1=tmp(1);
+obj.tau2=tmp(2);
+
+% localprob
+line=fgetl(fid);
+obj.flocalprob.fname=get_string(line);
+
+% rotation and affinity
+line=fgetl(fid);
+tmp=sscanf(line,'%d');
+obj.frotaff.use=tmp(1);
+
+line=fgetl(fid);
+obj.frotaff.fname=get_string(line);
+
+line=fgetl(fid);
+tmp=sscanf(line,'%d');
+obj.frotaff.n_cat=tmp(1);
+for i=1:obj.frotaff.n_cat
+    line=fgetl(fid);
+    tmp=sscanf(line,'%f %f %f');
+    obj.frotaff.aff_xyz(i,:)=tmp;
+end
+
+
+% n multiple grids
+line=fgetl(fid);
+data=sscanf(line,'%f');
+obj.n_mulgrids=data(1);
+
+% Training image
+line=fgetl(fid);
+obj.ti.fname=get_string(line);
+if (read_data>1)
+    obj.ti.data=read_eas(obj.ti.fname);
+end
+
+% ti dim
+line=fgetl(fid);
+data=sscanf(line,'%f');
+obj.ti.nx=data(1);
+obj.ti.ny=data(2);
+obj.ti.nz=data(3);
+
+if (read_data>1)
+    if obj.ti.nz==1
+        obj.ti.data=reshape(obj.ti.data,obj.ti.nx,obj.ti.ny)';
+    else
+        obj.ti.data=reshape(obj.ti.data,obj.ti.nx,obj.ti.ny,obj.ti.nz);
+    end
+end
+line=fgetl(fid);
+obj.ti.col_var=sscanf(line,'%f');
+
+% Search radius
+line=fgetl(fid);
+data=sscanf(line,'%f');
+obj.search_radius.hmax=data(1);
+obj.search_radius.hmin=data(2);
+obj.search_radius.hvert=data(3);
+% Search angles
+line=fgetl(fid);
+data=sscanf(line,'%f');
+obj.search_radius.amax=data(1);
+obj.search_radius.amin=data(2);
+obj.search_radius.avert=data(3);
+
+if (read_data>0)
+    nsim=obj.nsim;
+    nxyz=obj.dim.nx*obj.dim.ny*obj.dim.nz;
+    if (nsim>0)&(~isempty(obj.out.data))
+        %try
+        if obj.dim.nz==1,
+            obj.D=reshape(obj.out.data(1:(nsim*nxyz)),obj.dim.nx,obj.dim.ny,nsim)';
+        else
+            obj.D=reshape(obj.out.data(1:(nsim*nxyz)),obj.dim.nx,obj.dim.ny,obj.dim.nz,nsim);
+        end
+        if nsim==1
+            E=obj.D;
+            Ev=zeros(size(obj.D));
+        else
+            [E,Ev]=etype(obj.D);
+        end
+        obj.etype.mean=E;
+        obj.etype.var=Ev;
+        obj.nsim=nsim;
+        if (nsim~=obj.nsim),
+            disp(sprintf('SETTING NSIM=%d TO MATCH SIM DATA',nsim));
+        end
+        %catch
+        %    disp(sprintf('FAILED TO LOAD DATA',nsim));
+        %end
+    end
+end
+
+
+fclose(fid);
+
+
+return
+
+
+function [str_out]=get_string(str)
+
+fspace=find(str==' ');
+if length(fspace>0)
+    str_out=str(1:fspace(1)-1);
+else
+    str_out=str;
+end
+
