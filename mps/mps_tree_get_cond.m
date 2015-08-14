2dbcbd04ade12b440acad2964b294a9529c01e25
cultpenguin <cultpenguin@gmail.com>
2015-08-14 14:28:53 +0200

    added option in mps_tree_get_cond to return the pruned conditional used

--- mps/mps_tree_get_cond.m
+++ mps/mps_tree_get_cond.m
@@ -1,8 +1,23 @@
-% [c_pdf,c]=mps_tree_get_cond(ST,d_cond,cat);
+% mps_tree_get_cond: cond conditional from template
 %
-% see also: mps_tree_get_cond_notes
-function [c_pdf,c]=mps_tree_get_cond(ST,d_cond,cat)
-
+% Call:
+%   [c_pdf,c,d_cond_use]=mps_tree_get_cond(ST,d_cond);
+%
+%   INPUT
+%   ST: Search tree
+%   d_cond: conditional data event on order of the template
+%           unknown values are coded by 'NaN'
+%
+%   OUTPUT
+%   c_pdf: conditional probabily of outcome at center note given d_cond
+%   c: The counte matches leading to c_pdf
+%   d_cond_use: The actual conditinoal data event after pruning
+%
+%
+%
+% see also: mps_tree_get_cond_notes, mps_tree_populate
+%
+function [c_pdf,c,d_cond_use]=mps_tree_get_cond(ST,d_cond,cat)
 
 if nargin<2, d_cond=[];end
 if nargin<3, cat=0:1:(length(ST{1}.count)-1); end
@@ -25,6 +40,24 @@ for j=1:length(i_notes)
     c=c+ST{i_notes(j)}.count;
   end
 end
+
+
+% optionally prune if only veruy few counts are found
+doUsePruneForSmallCount=1;
+if doUsePruneForSmallCount==1;
+  
+  min_count=10;
+  if sum(c)<min_count
+    % d_cond(1:(length(d_cond)-1))
+    [c_pdf,c,d_cond_use]=mps_tree_get_cond(ST,d_cond(1:(length(d_cond)-1)));
+  else
+    d_cond_use=d_cond(use_cond);
+  end
+  
+else
+  d_cond_use=d_cond(use_cond);
+end
+
 
 c_pdf=c./sum(c);
 
