5498b67384d4233352edb8d9dffb1ae3bb87f288
Thomas Mejer Hansen <cultpenguin@gmail.com>
2016-06-23 16:51:50 +0200

    semivar_exp vecorization update

--- semivar_exp.m
+++ semivar_exp.m
@@ -58,33 +58,71 @@ h=zeros(nh,1);
 gamma=zeros(nh,ndata_types);
 vang=zeros(nh,1);
 
-i=0;
-for i1=1:(ndata-1)
-    for i2=(i1+1):ndata
-        i=i+1;
-        if ((i/100000)==round(i/100000))
-            progress_txt(i,nh,sprintf('semivar_exp : i=%d/%d',i,nh))
+
+usem=2;
+if usem==1
+    i=0;
+    for i1=1:(ndata-1)
+        for i2=(i1+1):ndata
+            i=i+1;
+            if ((i/100000)==round(i/100000))
+                progress_txt(i,nh,sprintf('semivar_exp : i=%d/%d',i,nh))
+            end
+            
+            p1=[pos(i1,:)];
+            p2=[pos(i2,:)];
+            pp=p1-p2;
+            
+            % distance
+            h(i)=sqrt(pp(1)^2+pp(2)^2);
+            % gamma
+            gamma(i,:)=0.5*(val(i1,:)-val(i2,:)).^2;
+            % angle
+            if pp(1)==0
+                vang(i)=0;
+            else
+                vang(i)=atan(pp(1)./pp(2));
+            end
+            
         end
+    end
+else
+    % vectorized
+    i=0;
+    for i1=1:(ndata-1)
+        
+        if ((i/1000)==round(i/1000))
+            progress_txt(i,nh,sprintf('semivar_exp : i=%d/%d',i1,ndata-1))
+        end
+        
+        
+        i2=(i1+1):ndata;
+        
+        i_arr=1:length(i2);
         
         p1=[pos(i1,:)];
         p2=[pos(i2,:)];
-        %dp(i,:)=p1-p2;
-        h(i)=sqrt( (p1-p2)*(p1-p2)' );
-        gamma(i,:)=0.5*(val(i1,:)-val(i2,:)).^2;
-        % ANGLE
-        pp=p1-p2;
-        % WORKS ONLY FOR 2D
-        if pp(1)==0
-            vang(i)=pi/2;
-        else
-            if (pp(1)>0);
-                
-            end
-            vang(i)=atan(pp(1)./pp(2));
-        end
+        pp=p2.*0;
+        pp(:,1)=p1(1)-p2(:,1);
+        pp(:,2)=p1(2)-p2(:,2);
+        
+        % distance
+        h(i+i_arr)=sqrt(pp(:,1).^2+pp(:,2).^2);
+        % gamma
+        gamma(i+i_arr,:)=0.5*(val(i1,:)-val(i2,:)).^2;
+        % angle
+        %if pp(1)==0
+        %    vang(i)=0;
+        %else
+            vang(i+i_arr)=atan(pp(:,1)./pp(:,2));
+        %end
+        
+        i=i+length(i_arr);
         
     end
 end
+
+
 ii=find(vang<0);vang(ii)=vang(ii)+pi;
 vang=180*vang/pi;
 
@@ -120,11 +158,11 @@ if exist('ang_array')==0
 end
 hangc=(ang_array(1:nbinang)+ang_array(2:nbinang+1))./2;
 
+
 clear garr
 for j=1:nbinang
     disp(sprintf('semivar_exp binning: i=%d/%d',i,nbinang))
     for i=1:nbin
-  
         f=find(h>=h_arr(i) & h<h_arr(i+1) & vang>=ang_array(j) & vang<ang_array(j+1));
         if (sum(gamma(f,:))==0)
             garr(i,j,:)=NaN;
