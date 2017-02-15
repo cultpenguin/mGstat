d3ba6cea2763b32f4d720d6976a3efb08210c342
Thomas Mejer Hansen <tmeha@nbi.ku.dk>
2017-02-15 16:26:23 +0100

    Fixing problems with opening file handles, when fopen is called in the same file often. Replace 'fopen' with 'fopen_retry' to fix this /tryuing again until success)

---
+++ misc/fopen_retry.m
@@ -0,0 +1,43 @@
+% fopen_retry: Retry opening file for writing until sucecssfull
+%              a replacement for fopen
+%
+% Call:
+%    fid=fopen_retry(filename,options,t_pause,N_max);
+%   
+%    filename: file to open
+%    options: e.g. 'w','r',....
+%    t_pause(1): pause in second after failure to open handle
+%    N_max(10): Number of tries....
+%
+% See also fopen
+%
+function fid=fopen_retry(filename,options,t_pause,N_max);
+if nargin<2, options='w';end
+if nargin<3, t_pause=1;end
+if nargin<4, N_max_try=10;end
+
+
+fid=-1;
+i=0;
+while fid<0
+    i=i+1;    
+    fid=fopen(filename,options);
+    
+    
+    if i==N_max_try
+        disp(sprintf('Tried to open %s %d times with no luck :/',filename,i))
+        break;
+    end
+    
+    if fid<0
+        disp(sprintf('failed to %s (%d time) trying again',filename,i));
+        pause(t_pause);
+    end
+    
+    
+end
+
+
+
+
+
