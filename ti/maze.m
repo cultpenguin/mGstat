d18368bda42ef6a9889d221983a948cd866786ec
Thomas Mejer Hansen <tmeha@nbi.ku.dk>
2017-11-10 14:11:36 +0100

    ..

--- ti/maze.m
+++ ti/maze.m
@@ -1,5 +1,11 @@
 % maze : 2D [810x810] training image
 % from From Dirk-Jan Kroon's fast marching toolbox
-function d=maze;
+function d=maze(tight);
 d=load('maze.mat');
 d=d.maze;
+
+if nargin<1, tight=1;end
+if tight==1;
+    % remove boundary
+    d=d(7:804,8:805);
+end
