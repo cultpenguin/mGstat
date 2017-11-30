b201c62235918d80a7af812889021cfc8fbf7922
Thomas Mejer Hansen <tmeha@nbi.ku.dk>
2017-11-30 11:15:06 +0100

    ScatterHist 2D update

--- scatterhist_2d.m
+++ scatterhist_2d.m
@@ -1,13 +1,27 @@
 % scatterhist_2d: 2D scatter histogram with 1d marginals.
 %
-%  scatterhist_2d(x_acc,y_acc)
-%  scatterhist_2d(x_acc,y_acc,x,y)
+%  scatterhist_2d(x_sample,y_sample);
+%  handles = scatterhist_2d(x_sample,y_sample,x_arr,y_arr);
 %
-function scatterhist_2d(x_acc,y_acc,x,y,plot_1d,gain)
+%  % example:
+%  x_sample = rand(1,12000);
+%  y_sample = randn(1,12000);
+%  
+%  subplot(1,2,1);
+%  scatterhist_2d(x_sample,y_sample);
+%  subplot(1,2,2);
+%  x_arr = linspace(-.1,1.1,31);
+%  y_arr = linspace(-5,5,31);
+%  scatterhist_2d(x_sample,y_sample,x_arr,y_arr);
+%
+% TMH/2017
+% 
+function [h,x,y]=scatterhist_2d(x_acc,y_acc,x,y,plot_1d,gain,linecolor)
 if nargin<3, x=linspace(min(x_acc),max(x_acc),51); end
 if nargin<4, y=linspace(min(y_acc),max(y_acc),51); end
 if nargin<5, plot_1d=1;end
-if nargin<6, gain=.3;end
+if nargin<6, gain=.8;end
+if nargin<7, linecolor=[1 0 0];end
 N=length(x_acc);
 nx=length(x);dx=x(2)-x(1);
 ny=length(y);dy=y(2)-y(1);
@@ -18,24 +32,25 @@ y0=y(1);
 lx=max(x)-min(x);
 ly=max(y)-min(y);
 
-[P_post_est,x_arr,y_arr] = hist2(x_acc(:),y_acc(:),x,y);
-imagesc(x,y,P_post_est');
-%colormap(1-gray);
-axis square
+%[P_post_est,x_arr,y_arr] = hist2(x_acc(:),y_acc(:),x,y);
+%h=imagesc(x,y,P_post_est');colormap(1-gray);axis image
+
+P=histcounts2(x_acc(:),y_acc(:),x,y);
+x_c=(x(2:end)+x(1:end-1))/2;
+y_c=(y(2:end)+y(1:end-1))/2;
+h(1)=imagesc(x_c,y_c,P');
+
 xlabel('m_1');ylabel('m_2')
 set(gca,'ydir','normal')
 ax=axis;
 if plot_1d==1;
     hold on
-    
-    Px=hist(x_acc,x);Px=Px/max(Px);
-    Py=hist(y_acc,y);Py=Py/max(Py);
-    
-    g=1;
-    %g=0.5;
-    plot(x,y0+zeros(1,nx)+gain*ly.*Px,'-','color',[1 1 1].*g,'LineWidth',2)
-    plot(x0+zeros(1,ny)+gain*lx.*Py,y,'-','color',[1 1 1].*g,'LineWidth',2)
+    Px=hist(x_acc,x)/(dx*N);
+    Py=hist(y_acc,y)/(dy*N);
+    h(2)=plot(x,y0+zeros(1,nx)+gain*ly.*Px,'-','color',linecolor);
+    h(3)=plot(x0+zeros(1,ny)+gain*lx.*Py,y,'-','color',linecolor);
     hold off
 end
 
 axis(ax);
+axis square;
