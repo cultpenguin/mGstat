69f1dc53d81a3585415631ab1df6308820513e98
Thomas Mejer Hansen <Thomas.Mejer.Hansen@gmail.com>
2023-06-16 09:30:29 +0200

    MemInfo

--- misc/memory_get.m
+++ misc/memory_get.m
@@ -14,6 +14,13 @@ if isunix
     mem_total=str2num(ws{2})/1e+6;
     mem_used=str2num(ws{3})/1e+6;
     mem_available=str2num(ws{4})/1e+6;
+
+    % On Ubuntu
+    [r,w]=unix('cat /proc/meminfo | grep MemFree');i1=strfind(w,'MemFree');ws=split(w((i1):end));mem_available=str2num(ws{2})/1e+6;
+    [r,w]=unix('cat /proc/meminfo | grep MemTotal');i1=strfind(w,'MemTotal');ws=split(w((i1):end));mem_total=str2num(ws{2})/1e+6;
+    [r,w]=unix('cat /proc/meminfo | grep MemAvailable');i1=strfind(w,'MemAvailable');ws=split(w((i1):end));mem_available=str2num(ws{2})/1e+6;
+    mem_used = mem_total-mem_available;
+    
 else 
     [USERVIEW, SYSTEMVIEW] = memory;
     mem_total=SYSTEMVIEW.PhysicalMemory.Total/1024/1E+6;
